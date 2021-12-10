9daf3d41c0f7c840a0508230f070b185f8d808a7
Lionel Parreaux <lionel.parreaux@gmail.com>
2021-12-10 18:57:05 +0800

    Merge unions of tuple types in type normalizer

--- shared/src/main/scala/mlscript/TyperHelpers.scala
+++ shared/src/main/scala/mlscript/TyperHelpers.scala
@@ -68,6 +68,13 @@ abstract class TyperHelpers { self: Typer =>
       case ((no1, t1), (no2, t2)) => (no1 orElse no2, t1 & t2)
     }
   }
+  def tupleUnion(fs1: Ls[Opt[Var] -> SimpleType], fs2: Ls[Opt[Var] -> SimpleType]): Ls[Opt[Var] -> SimpleType] = {
+    require(fs1.size === fs2.size)
+    (fs1 lazyZip fs2).map {
+      case ((S(n1), t1), (S(n2), t2)) => (Option.when(n1 === n2)(n1), t1 | t2)
+      case ((no1, t1), (no2, t2)) => (N, t1 | t2)
+    }
+  }
   
   trait SimpleTypeImpl { self: SimpleType =>
     
