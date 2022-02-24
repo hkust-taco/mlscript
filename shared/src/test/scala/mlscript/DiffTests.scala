82428070aa2ac3762d8f2523a491cf519c755473
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2022-02-24 19:38:36 +0800

    Update shared/src/test/scala/mlscript/DiffTests.scala
    Co-authored-by: Lionel Parreaux <lionel.parreaux@gmail.com>

--- shared/src/test/scala/mlscript/DiffTests.scala
+++ shared/src/test/scala/mlscript/DiffTests.scala
@@ -57,7 +57,7 @@ class DiffTests extends org.scalatest.funsuite.AnyFunSuite with org.scalatest.Pa
       fixme: Bool, showParse: Bool, verbose: Bool, noSimplification: Bool,
       explainErrors: Bool, dbg: Bool, fullExceptionStack: Bool, stats: Bool,
       stdout: Bool, noExecution: Bool, noGeneration: Bool, showGeneratedJS: Bool,
-      showDeclarationTS: Bool, expectRuntimeErrors: Bool, showRepl: Bool, allowEscape: Bool)
+      generateTsDeclarations: Bool, expectRuntimeErrors: Bool, showRepl: Bool, allowEscape: Bool)
     val defaultMode = Mode(false, false, false, false, false, false, false, false,
       false, false, false, false, false, false, false, false, false, false, false)
     
