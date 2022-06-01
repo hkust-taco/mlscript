521807e166b4dd8ecdcc21407c6c71587f35bd9e
Lionel Parreaux <lionel.parreaux@gmail.com>
2022-06-01 13:22:42 +0800

    WIP start porting lexer

---
+++ shared/src/main/scala/mlscript/NewLexer.scala
@@ -0,0 +1,161 @@
+package mlscript
+
+
+import scala.annotation.tailrec
+import utils._, shorthands._
+
+// import Token._
+import Message.MessageContext
+
+class NewLexer(origin: Origin, raise: Diagnostic => Unit, dbg: Bool) {
+
+  type TokLoc = (Token, Loc)
+  
+  val bytes: Array[Char] = origin.fph.blockStr.toArray
+  val length = bytes.length
+  type State = Int
+  
+  val isOpChar = Set(
+    '!', '#', '%', '&', '*', '+', '-', '/', ':', '<', '=', '>', '?', '@', '\\', '^', '|', '~' , '.', ',', ';'
+  )
+  def isIdentFirstChar(c: Char): Bool =
+    c.isLetter || c == '_' || c == '\''
+  def isIdentChar(c: Char): Bool =
+    isIdentFirstChar(c) || isDigit(c) || c == '\''
+  def isDigit(c: Char): Bool =
+    c >= '0' && c <= '9'
+  
+  val isNonStickyKeywordChar = Set(
+    ',',
+    ':',
+    ';',
+  )
+  
+  val isSymKeyword = Set(
+    "->",
+    "=",
+    ":",
+    ";",
+  )
+  
+  // val keywords = Map(
+  //   "data" -> Pre,
+  //   "class" -> Pre,
+  //     "with" -> In,
+  //     "extends" -> In,
+  //   "if" -> Pre,
+  //     "then" -> In,
+  //     "else" -> In,
+  //   "and" -> In,
+  //   "or" -> In,
+  //   "match" -> In,
+  //   "as" -> In,
+  //   "let" -> Pre,
+  //   // "_" -> In,
+  // )
+  
+  @tailrec final
+  def takeWhile(i: Int, cur: Ls[Char] = Nil)(pred: Char => Bool): (Str, Int) =
+    if (i < length && pred(bytes(i))) takeWhile(i + 1, bytes(i) :: cur)(pred)
+    else (cur.reverseIterator.mkString, i)
+  
+  def loc(start: Int, end: Int): Loc = Loc(start, end, origin)
+  
+  // @tailrec final
+  def lex(i: Int, ind: Ls[Int], acc: Ls[TokLoc]): Ls[TokLoc] = if (i >= length) acc.reverse else {
+    val c = bytes(i)
+    def pe(msg: Message): Unit =
+      // raise(ParseError(false, msg -> S(loc(i, i + 1)) :: Nil))
+      raise(TypeError(msg -> S(loc(i, i + 1)) :: Nil)) // TODO parse error
+    // @inline 
+    def go(j: Int, tok: Token) = lex(j, ind, (tok, loc(i, j)) :: acc)
+    c match{
+      case ' ' =>
+        val (_, j) = takeWhile(i)(_ === ' ')
+        go(j, SPACE())
+      case '"' =>
+        val j = i + 1
+        val (chars, k) = takeWhile(j)(c => c =/= '"' && c =/= '\n')
+        val k2 = if (bytes.lift(k) === Some('"')) k + 1 else {
+          pe(msg"unclosed quotation mark")
+          k
+        }
+        go(k2, LITERAL(StrLit(chars)))
+      case '/' if bytes.lift(i + 1).contains('/') =>
+        val j = i + 2
+        val (txt, k) =
+          takeWhile(j)(c => c =/= '\n')
+        go(k, COMMENT(txt))
+      case BracketKind(Left(k)) => go(i + 1, OPEN_BRACKET(k))
+      case BracketKind(Right(k)) => go(i + 1, CLOSE_BRACKET(k))
+      case '\n' =>
+        val j = i + 1
+        val (space, k) =
+          takeWhile(j)(c => c === ' ' || c === '\n')
+        val nextInd = space.reverseIterator.takeWhile(_ =/= '\n').size
+        if (ind.headOption.forall(_ < nextInd) && nextInd > 0)
+          lex(k, nextInd :: ind, (INDENT(), loc(j, k)) :: acc)
+        else {
+          val newIndBase = ind.dropWhile(_ > nextInd)
+          val droppedNum = ind.size - newIndBase.size
+          val hasNewIndent = newIndBase.headOption.forall(_ < nextInd) && nextInd > 0
+          val newInd = if (hasNewIndent) nextInd :: newIndBase else newIndBase
+          if (dbg) {
+            println("dbg: " + bytes.drop(i).take(10).map(escapeChar).mkString+"...")
+            println((ind, nextInd, newIndBase, droppedNum, hasNewIndent, newInd))
+          }
+          lex(k, newInd,
+            if (droppedNum > 0) {
+              if (hasNewIndent) (INDENT(), loc(j, k))
+              else (NEWLINE(), loc(i, k))
+            } :: List.fill(droppedNum)((DEINDENT(), loc(j, k))) ::: acc
+            else (NEWLINE(), loc(i, k)) :: acc
+          )
+        }
+      case _ if isIdentFirstChar(c) =>
+        val (n, j) = takeWhile(i)(isIdentChar)
+        go(j, IDENT(n, false))
+      case _ if isOpChar(c) =>
+        val (n, j) = takeWhile(i)(isOpChar)
+        go(j, IDENT(n, true))
+      case _ if isDigit(c) =>
+        val (str, j) = takeWhile(i)(isDigit)
+        go(j, LITERAL(IntLit(BigInt(str))))
+      case _ =>
+        pe(msg"unexpected character '${escapeChar(c)}'")
+        go(i + 1, ERROR())
+    }
+  }
+ 
+  def escapeChar(ch: Char): String = ch match {
+    case '\b' => "\\b"
+    case '\t' => "\\t"
+    case '\n' => "\\n"
+    case '\f' => "\\f"
+    case '\r' => "\\r"
+    case '"'  => "\\\""
+    case '\'' => "\\\'"
+    case '\\' => "\\\\"
+    case _    => if (ch.isControl) "\\0" + Integer.toOctalString(ch.toInt) 
+                else              String.valueOf(ch)
+  }
+  
+  
+  def tokens: Ls[Token -> Loc] = lex(0, Nil, Nil)
+  
+  def printTokens(ts: Ls[TokLoc]): Str = "|" + (ts match {
+    // case (SPACE, _) :: ts => " " + printTokens(ts)
+    // case (NEWLINE, _) :: ts => "↵" + printTokens(ts)
+    // case (INDENT, _) :: ts => "→" + printTokens(ts)
+    // case (DEINDENT, _) :: ts => "←" + printTokens(ts)
+    // case (ERROR, _) :: ts => "<error>"
+    // case (LIT(lv), _) :: ts => lv.identifyingStr + printTokens(ts)
+    // case (IDENT(name: String, symbolic: Bool), _) :: ts => name + printTokens(ts)
+    // case (OPEN_BRACKET(k: BracketKind), _) :: ts => k.beg.toString + printTokens(ts)
+    // case (CLOSE_BRACKET(k: BracketKind), _) :: ts => k.end.toString + printTokens(ts)
+    // case (COMMENT(text: String), _) :: ts => "/*" + text + "*/"
+    case Nil => ""
+  })
+  
+
+}
---
+++ shared/src/main/scala/mlscript/Token.scala
@@ -0,0 +1,49 @@
+package mlscript
+
+import mlscript.utils._, shorthands._
+
+
+sealed abstract class Token
+
+final case class SPACE() extends Token
+final case class NEWLINE() extends Token // TODO rm
+final case class INDENT() extends Token // TODO rm
+final case class DEINDENT() extends Token // TODO rm
+final case class INDENTED(block: Ls[Token]) extends Token
+final case class ERROR() extends Token
+final case class LITERAL(value: Lit) extends Token
+final case class IDENT(name: String, symbolic: Bool) extends Token
+final case class OPEN_BRACKET(k: BracketKind) extends Token // TODO rm
+final case class CLOSE_BRACKET(k: BracketKind) extends Token // TODO rm
+final case class BRACKETS(k: BracketKind) extends Token
+final case class COMMENT(text: String) extends Token
+
+
+sealed abstract class BracketKind {
+  import BracketKind._
+  lazy val (beg, end) = this match {
+    case Round => '(' -> ')'
+    case Curly => '{' -> '}'
+    case Square => '[' -> ']'
+  }
+  def name = this match {
+    case Round => "parenthesis"
+    case Curly => "curly brace"
+    case Square => "square bracket"
+  }
+}
+
+object BracketKind {
+  case object Round extends BracketKind
+  case object Curly extends BracketKind
+  case object Square extends BracketKind
+  def unapply(c: Char): Opt[Either[BracketKind, BracketKind]] = c |>? {
+    case '(' => Left(Round)
+    case ')' => Right(Round)
+    case '{' => Left(Curly)
+    case '}' => Right(Curly)
+    case '[' => Left(Square)
+    case ']' => Right(Square)
+  }
+}
+
