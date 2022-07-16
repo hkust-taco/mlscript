07140088b5eae36aadfe99b91c7b6c95c2828cd4
Luyu Cheng <chengluyu@live.cn>
2022-07-16 21:35:38 +0800

    Add a work-in-progress `Converter`

--- mono/shared/main/scala/mlscript/mono/syntax.scala
+++ mono/shared/main/scala/mlscript/mono/syntax.scala
@@ -1,5 +1,9 @@
 package mlscript.mono
 
+import mlscript.{Type, Union, Inter, Function, Record, Tuple, Recursive, AppliedType,
+                 Neg, Rem, Bounds, WithExtension, Constrained, Top, Bot, Literal,
+                 TypeName, TypeVar, PolyType}
+
 enum Expr:
   case Ref(name: String)
   case Lambda(params: List[Ref], body: Expr)
@@ -11,15 +15,23 @@ enum Expr:
   case Assign(assignee: Expr, value: Expr)
   case Subscript(receiver: Expr, index: Expr)
   case Match(scrutinee: Expr, branches: CaseBranch)
-  enum Literal:
-    case Integer(value: BigInt)
-    case Decimal(value: BigDecimal)
-    case String(value: Predef.String)
-    case Unit(undefinedOrNull: Boolean)
-  end Literal
+  case Literal(value: BigInt | BigDecimal | String)
+  case New(apply: Option[(TypeName, List[Expr])], body: Isolation)
 end Expr
 
 enum CaseBranch:
-  case Pattern(pattern: Expr.Ref, body: Expr, tail: CaseBranch)
+  case Pattern(pattern: Expr.Ref | Expr.Literal, body: Expr, tail: CaseBranch)
   case Wildcard(body: Expr)
   case Empty
+end CaseBranch
+
+enum DeclKind:
+  case Alias, Class, Trait
+
+enum Item:
+  case TypeDecl(name: TypeName, kind: DeclKind, typeParams: List[TypeName],
+                parents: List[AppliedType], body: Isolation)
+  case FuncDecl(name: Expr.Ref, body: Expr)
+  case FuncDefn(name: Expr.Ref, typeParams: List[TypeName], body: PolyType)
+
+class Isolation(items: List[Expr | Item])
