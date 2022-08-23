64850ce13575cdf3b769c5fd8aa23e1e2d15f567
NeilKleistGao <neilkleist@gmail.com>
2022-08-23 14:52:57 +0800

    Update CI

--- ts2mls/jvm/src/test/scala/ts2mls/TsTypeDiffTests.scala
+++ ts2mls/jvm/src/test/scala/ts2mls/TsTypeDiffTests.scala
@@ -4,12 +4,6 @@ import mlscript.DiffTests
 import mlscript.utils._, shorthands._
 
 class TsTypeDiffTests extends DiffTests {
-  import TsTypeDiffTests._
-
   override protected def getFiles() =
-    os.walk(dir).filter(_.toIO.isFile)
-}
-
-object TsTypeDiffTests {
-  private val dir = os.pwd/"ts2mls"/"js"/"src"/"test"/"diff"
+    os.walk(os.pwd/"ts2mls"/"js"/"src"/"test"/"diff").filter(_.toIO.isFile)
 }
