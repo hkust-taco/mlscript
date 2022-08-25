1a44df90869824cfb521b00638060a51bd673dac
NeilKleistGao <neilkleist@gmail.com>
2022-08-25 09:35:50 +0800

    Fix writer

--- ts2mls/js/src/main/scala/ts2mls/JSWriter.scala
+++ ts2mls/js/src/main/scala/ts2mls/JSWriter.scala
@@ -5,15 +5,25 @@ import js.Dynamic.{global => g}
 import js.DynamicImplicits._
 
 class JSWriter(filename: String) {
-  import JSWriter.fs
+  import JSWriter.{fs, createBuffer}
   private val out = fs.openSync(filename, "w+")
 
-  def writeln(str: String): Unit = fs.writeSync(out, s"$str\n")
+  def writeln(str: String): Unit = {
+    val strln = s"$str\n"
+    val buffer = createBuffer(strln.length)
+    fs.readSync(out, buffer, 0, strln.length)
+    if (!strln.equals(buffer.toString()))
+      fs.writeSync(out, strln, -strln.length)
+  }
+
   def close(): Unit = fs.closeSync(out)
 }
 
 object JSWriter {
   private val fs = g.require("fs") // must use fs module to manipulate files in JS
+  // private val buffer = g.require("buffer")
 
   def apply(filename: String) = new JSWriter(filename)
+
+  private def createBuffer(length: Int) = g.Buffer.alloc(length)
 }
