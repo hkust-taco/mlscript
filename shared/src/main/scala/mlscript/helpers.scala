hkust-taco/mlstruct

Parse and print new type tag syntax

shared/src/main/scala/mlscript/helpers.scala
@@ -39,6 +39,7 @@ abstract class TypeImpl extends Located { self: Type =>
     case Top => "anything"
     case Bot => "nothing"
     case TypeName(name) => name
+    case TypeTag(name) => "#"+name
     case uv: TypeVar => ctx.vs(uv)
     case Recursive(n, b) => parensIf(s"${b.showIn(ctx, 2)} as ${ctx.vs(n)}", outerPrec > 1)
     case Function(Tuple((N, l) :: Nil), r) => Function(l, r).showIn(ctx, outerPrec)
@@ -121,7 +122,7 @@ abstract class TypeImpl extends Located { self: Type =>
     case Inter(ty1, ty2) => ty1.collectFields ++ ty2.collectFields
     case _: Union | _: Function | _: Tuple | _: Recursive
         | _: Neg | _: Bounds | Top | Bot
-        | _: Literal | _: TypeVar | _: AppliedType | _: TypeName | _: Constrained =>
+        | _: Literal | _: TypeVar | _: AppliedType | _: TypeName | _: TypeName | _: TypeTag | _: Constrained =>
       Nil
   }
 
@@ -134,7 +135,7 @@ abstract class TypeImpl extends Located { self: Type =>
     case AppliedType(TypeName(name), _) => name :: Nil
     case Inter(lhs, rhs) => lhs.collectTypeNames ++ rhs.collectTypeNames
     case _: Union | _: Function | _: Record | _: Tuple | _: Recursive
-        | _: Neg | _: Bounds | Top | Bot
+        | _: Neg | _: Bounds | Top | Bot | _: TypeTag
         | _: Literal | _: TypeVar | _: Constrained =>
       Nil
   }
@@ -148,7 +149,7 @@ abstract class TypeImpl extends Located { self: Type =>
     case Inter(ty1, ty2) => ty1.collectBodyFieldsAndTypes ++ ty2.collectBodyFieldsAndTypes
     case _: Union | _: Function | _: Tuple | _: Recursive
         | _: Neg | _: Bounds | Top | Bot
-        | _: Literal | _: TypeVar | _: AppliedType | _: TypeName | _: Constrained =>
+        | _: Literal | _: TypeVar | _: AppliedType | _: TypeName | _: TypeTag | _: Constrained =>
       Nil
   }
 }

hkust-taco/mlscript

shared/src/main/scala/mlscript/helpers.scala
@@ -45,6 +45,7 @@ abstract class TypeImpl extends Located { self: Type =>
     case Bot => "nothing"
     case TypeName(name) => name
     // case uv: TypeVar => ctx.vs.getOrElse(uv, s"[??? $uv ???]")
+    case TypeTag(name) => "#"+name
     case uv: TypeVar => ctx.vs(uv)
     case Recursive(n, b) => parensIf(s"${b.showIn(ctx, 2)} as ${ctx.vs(n)}", outerPrec > 1)
     case WithExtension(b, r) => parensIf(s"${b.showIn(ctx, 2)} with ${r.showIn(ctx, 0)}", outerPrec > 1)
@@ -135,7 +136,7 @@ abstract class TypeImpl extends Located { self: Type =>
     case _: Union | _: Function | _: Tuple | _: Recursive
         | _: Neg | _: Rem | _: Bounds | _: WithExtension | Top | Bot
         | _: Literal | _: TypeVar | _: AppliedType | _: TypeName 
-        | _: Constrained | _ : Splice =>
+        | _: Constrained | _ : Splice | _: TypeTag =>
       Nil
   }
 
@@ -149,7 +150,7 @@ abstract class TypeImpl extends Located { self: Type =>
     case Inter(lhs, rhs) => lhs.collectTypeNames ++ rhs.collectTypeNames
     case _: Union | _: Function | _: Record | _: Tuple | _: Recursive
         | _: Neg | _: Rem | _: Bounds | _: WithExtension | Top | Bot
-        | _: Literal | _: TypeVar | _: Constrained | _ : Splice =>
+        | _: Literal | _: TypeVar | _: Constrained | _ : Splice | _: TypeTag =>
       Nil
   }
 
@@ -162,7 +163,7 @@ abstract class TypeImpl extends Located { self: Type =>
     case Inter(ty1, ty2) => ty1.collectBodyFieldsAndTypes ++ ty2.collectBodyFieldsAndTypes
     case _: Union | _: Function | _: Tuple | _: Recursive
         | _: Neg | _: Rem | _: Bounds | _: WithExtension | Top | Bot
-        | _: Literal | _: TypeVar | _: AppliedType | _: TypeName | _: Constrained | _ : Splice =>
+        | _: Literal | _: TypeVar | _: AppliedType | _: TypeName | _: Constrained | _ : Splice | _: TypeTag =>
       Nil
   }
 }