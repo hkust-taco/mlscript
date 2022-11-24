d364bf1e059d2207d2846d4b8b5a66d1e5ae359e
Luyu Cheng <chengluyu@live.cn>
2022-11-24 19:42:06 +0800

    Remove duplicated code in `PartialTerm`

--- shared/src/main/scala/mlscript/ucs/PartialTerm.scala
+++ shared/src/main/scala/mlscript/ucs/PartialTerm.scala
@@ -34,23 +34,13 @@ object PartialTerm {
   }
 
   final case class Half(lhs: Term, op: Var, override val fragments: Ls[Term]) extends PartialTerm(fragments) {
-    override def addTerm(rhs: Term): Total = op match {
-      // TODO: Why are they look the same? How did the code here evolve?
-      case isVar @ Var("is") =>
-        // Special treatment for pattern matching.
-        val (pattern, extraTestOpt) = separatePattern(rhs)
-        val assertion = mkBinOp(lhs, isVar, pattern)
-        extraTestOpt match {
-          case N            => Total(assertion, fragments)
-          case S(extraTest) => Total(mkBinOp(assertion, Var("and"), extraTest), extraTest :: fragments)
-        }
-      case _ =>
-        val (realRhs, extraExprOpt) = separatePattern(rhs)
-        val leftmost = mkBinOp(lhs, op, realRhs)
-        extraExprOpt match {
-          case N            => Total(leftmost, fragments)
-          case S(extraExpr) => Total(mkBinOp(leftmost, Var("and"), extraExpr), extraExpr :: fragments)
-        }
+    override def addTerm(rhs: Term): Total = {
+      val (realRhs, extraExprOpt) = separatePattern(rhs)
+      val leftmost = mkBinOp(lhs, op, realRhs)
+      extraExprOpt match {
+        case N            => Total(leftmost, fragments)
+        case S(extraExpr) => Total(mkBinOp(leftmost, Var("and"), extraExpr), extraExpr :: fragments)
+      }
     }
     override def addOp(op: Var): Half =
       throw new PartialTermError(this, s"expect a term but operator $op was given")
