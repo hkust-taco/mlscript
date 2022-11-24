cf98107279d4ac40d07ef954def468abb059bb45
Lionel Parreaux <lionel.parreaux@gmail.com>
2022-11-24 12:43:05 +0800

    WIP

--- shared/src/main/scala/mlscript/ConstraintSolver.scala
+++ shared/src/main/scala/mlscript/ConstraintSolver.scala
@@ -299,13 +299,14 @@ class ConstraintSolver extends NormalForms { self: Typer =>
       // println(s"[[ ${cctx._1.map(_.prov).mkString(", ")}  <<  ${cctx._2.map(_.prov).mkString(", ")} ]]")
       // println(s"{{ ${cache.mkString(", ")} }}")
       
-      if (lhs === rhs) return ()
+      // if (lhs === rhs) return ()
+      if (false) return ()
       
       // if (lhs <:< rhs) return () // * It's not clear that doing this here is worth it
       
       // println(s"  where ${FunctionType(lhs, rhs)(primProv).showBounds}")
       else {
-        if (lhs is rhs) return
+        // if (lhs is rhs) return
         val lhs_rhs = lhs -> rhs
         lhs_rhs match {
           case (_: ProvType, _) | (_, _: ProvType) => ()
@@ -325,6 +326,7 @@ class ConstraintSolver extends NormalForms { self: Typer =>
           case (_, TypeBounds(lb, ub)) => rec(lhs, lb, true)
           case (p @ ProvType(und), _) => rec(und, rhs, true)
           case (_, p @ ProvType(und)) => rec(lhs, und, true)
+          case (_: ObjectTag, _: ObjectTag) if lhs === rhs => ()
           case (NegType(lhs), NegType(rhs)) => rec(rhs, lhs, true)
           case (FunctionType(l0, r0), FunctionType(l1, r1)) =>
             rec(l1, l0, false)
