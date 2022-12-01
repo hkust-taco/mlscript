86792217c49e508bf4be8a597376a80513f1ff6f
NeilKleistGao <neilkleist@gmail.com>
2022-12-01 17:14:39 +0800

    Use ascribe instead

--- ts2mls/js/src/main/scala/ts2mls/types/Converter.scala
+++ ts2mls/js/src/main/scala/ts2mls/types/Converter.scala
@@ -65,8 +65,8 @@ object Converter {
           case _: TSFunctionType => s"${generateFunDeclaration(m._2.base, m._1)(indent + "  ")}\n"
           case _: TSIgnoredOverload => s"${generateFunDeclaration(m._2.base, m._1)(indent + "  ")}\n"
           case _ =>
-            if (m._2.mutable) s"${indent}  let ${m._1}: {mut contents: ${convert(m._2)}}\n"
-            else s"${indent}  let ${m._1}: ${convert(m._2)}\n"
+            if (m._2.mutable) s"${indent}  ${m._1}: {mut contents: ${convert(m._2)}}\n"
+            else s"${indent}  ${m._1}: ${convert(m._2)}\n"
         }
       case _ => "" // TODO: deal with private/protected members
     }) :::
