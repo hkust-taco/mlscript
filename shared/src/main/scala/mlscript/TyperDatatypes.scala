0fcbff11c18123561308a457940c23167a13ae9d
Lionel Parreaux <lionel.parreaux@gmail.com>
2023-01-20 14:25:59 +0800

    WIP justify an OC failure

--- shared/src/main/scala/mlscript/TyperDatatypes.scala
+++ shared/src/main/scala/mlscript/TyperDatatypes.scala
@@ -646,13 +646,13 @@ abstract class TyperDatatypes extends TyperHelpers { self: Typer =>
       * Note that if we have something like 'a :> Bot <: 'a -> Top, 'a is not truly recursive
       *   and its bounds can actually be inlined. */
     private final def lbRecOccs_$(omitTopLevel: Bool)(implicit ctx: Ctx): Opt[Opt[Bool]] = {
-      println("+", this, assignedTo getOrElse lowerBounds)
+      // println("+", this, assignedTo getOrElse lowerBounds)
       // assignedTo.getOrElse(TupleType(lowerBounds.map(N -> _.toUpper(noProv)))(noProv)).getVarsPol(PolMap.pos, ignoreTopLevelOccs = true).get(this)
       val bs = assignedTo.fold(lowerBounds)(_ :: Nil)
       bs.foldLeft(BotType: ST)(_ | _).getVarsPol(PolMap.pos, ignoreTopLevelOccs = omitTopLevel).get(this)
     }
     private final def ubRecOccs_$(omitTopLevel: Bool)(implicit ctx: Ctx): Opt[Opt[Bool]] ={
-      println("-", this, assignedTo getOrElse upperBounds)
+      // println("-", this, assignedTo getOrElse upperBounds)
       // assignedTo.getOrElse(TupleType(upperBounds.map(N -> _.toUpper(noProv)))(noProv)).getVarsPol(PolMap.posAtNeg, ignoreTopLevelOccs = true).get(this)
       val bs = assignedTo.fold(upperBounds)(_ :: Nil)
       bs.foldLeft(TopType: ST)(_ & _).getVarsPol(PolMap.posAtNeg, ignoreTopLevelOccs = omitTopLevel).get(this)
