728880acca5fe111bbfb2a5b64406a3a0488b2aa
NeilKleistGao <neilkleist@gmail.com>
2023-06-20 17:21:57 +0800

    WIP: Add more content in es5

--- ts2mls/js/src/test/typescript/ES5.d.ts
+++ ts2mls/js/src/test/typescript/ES5.d.ts
@@ -423,27 +423,27 @@ class Str extends Object {
      * Matches a string with a regular expression, and returns an array containing the results of that search.
      * @param regexp A variable name or string literal containing the regular expression pattern and flags.
      */
-    // match(regexp: string | RegExp): RegExpMatchArray | null;
+    match(regexp: string | RegExp): RegExpMatchArray | null;
 
     /**
      * Replaces text in a string, using a regular expression or search string.
      * @param searchValue A string or regular expression to search for.
      * @param replaceValue A string containing the text to replace. When the {@linkcode searchValue} is a `RegExp`, all matches are replaced if the `g` flag is set (or only those matches at the beginning, if the `y` flag is also present). Otherwise, only the first match of {@linkcode searchValue} is replaced.
      */
-    // replace(searchValue: string | RegExp, replaceValue: string): string;
+    replace(searchValue: string | RegExp, replaceValue: string): string;
 
     /**
      * Replaces text in a string, using a regular expression or search string.
      * @param searchValue A string to search for.
      * @param replacer A function that returns the replacement text.
      */
-    // replace(searchValue: string | RegExp, replacer: (substring: string, ...args: any[]) => string): string;
+    replace(searchValue: string | RegExp, replacer: (substring: string, ...args: any[]) => string): string;
 
     /**
      * Finds the first substring match in a regular expression search.
      * @param regexp The regular expression pattern and applicable flags.
      */
-    // search(regexp: string | RegExp): number;
+    search(regexp: string | RegExp): number;
 
     /**
      * Returns a section of a string.
@@ -458,7 +458,7 @@ class Str extends Object {
      * @param separator A string that identifies character or characters to use in separating the string. If omitted, a single-element array containing the entire string is returned.
      * @param limit A value used to limit the number of elements returned in the array.
      */
-    // split(separator: string | RegExp, limit?: number): string[];
+    split(separator: string | RegExp, limit?: number): string[];
 
     /**
      * Returns the substring at the specified location within a String object.
@@ -921,117 +921,116 @@ interface DateConstructor {
 
 // declare var Date: DateConstructor;
 
-// TODO: 0?
-// interface RegExpMatchArray extends MutArray<string> {
-//     /**
-//      * The index of the search at which the result was found.
-//      */
-//     index?: number;
-//     /**
-//      * A copy of the search string.
-//      */
-//     input?: string;
-//     /**
-//      * The first match. This will always be present because `null` will be returned if there are no matches.
-//      */
-//     0: string;
-// }
+interface RegExpMatchArray extends MutArray<string> {
+    /**
+     * The index of the search at which the result was found.
+     */
+    index?: number;
+    /**
+     * A copy of the search string.
+     */
+    input?: string;
+    /**
+     * The first match. This will always be present because `null` will be returned if there are no matches.
+     */
+    0: string;
+}
 
-// interface RegExpExecArray extends MutArray<string> {
-//     /**
-//      * The index of the search at which the result was found.
-//      */
-//     index: number;
-//     /**
-//      * A copy of the search string.
-//      */
-//     input: string;
-//     /**
-//      * The first match. This will always be present because `null` will be returned if there are no matches.
-//      */
-//     0: string;
-// }
+interface RegExpExecArray extends MutArray<string> {
+    /**
+     * The index of the search at which the result was found.
+     */
+    index: number;
+    /**
+     * A copy of the search string.
+     */
+    input: string;
+    /**
+     * The first match. This will always be present because `null` will be returned if there are no matches.
+     */
+    0: string;
+}
 
-// interface RegExp {
-//     /**
-//      * Executes a search on a string using a regular expression pattern, and returns an array containing the results of that search.
-//      * @param string The String object or string literal on which to perform the search.
-//      */
-//     exec(string: string): RegExpExecArray | null;
+interface RegExp {
+    /**
+     * Executes a search on a string using a regular expression pattern, and returns an array containing the results of that search.
+     * @param string The String object or string literal on which to perform the search.
+     */
+    exec(string: string): RegExpExecArray | null;
 
-//     /**
-//      * Returns a Boolean value that indicates whether or not a pattern exists in a searched string.
-//      * @param string String on which to perform the search.
-//      */
-//     test(string: string): boolean;
+    /**
+     * Returns a Boolean value that indicates whether or not a pattern exists in a searched string.
+     * @param string String on which to perform the search.
+     */
+    test(string: string): boolean;
 
-//     /** Returns a copy of the text of the regular expression pattern. Read-only. The regExp argument is a Regular expression object. It can be a variable name or a literal. */
-//     readonly source: string;
+    /** Returns a copy of the text of the regular expression pattern. Read-only. The regExp argument is a Regular expression object. It can be a variable name or a literal. */
+    readonly source: string;
 
-//     /** Returns a Boolean value indicating the state of the global flag (g) used with a regular expression. Default is false. Read-only. */
-//     readonly global: boolean;
+    /** Returns a Boolean value indicating the state of the global flag (g) used with a regular expression. Default is false. Read-only. */
+    readonly global: boolean;
 
-//     /** Returns a Boolean value indicating the state of the ignoreCase flag (i) used with a regular expression. Default is false. Read-only. */
-//     readonly ignoreCase: boolean;
+    /** Returns a Boolean value indicating the state of the ignoreCase flag (i) used with a regular expression. Default is false. Read-only. */
+    readonly ignoreCase: boolean;
 
-//     /** Returns a Boolean value indicating the state of the multiline flag (m) used with a regular expression. Default is false. Read-only. */
-//     readonly multiline: boolean;
+    /** Returns a Boolean value indicating the state of the multiline flag (m) used with a regular expression. Default is false. Read-only. */
+    readonly multiline: boolean;
 
-//     lastIndex: number;
+    lastIndex: number;
 
-//     // Non-standard extensions
-//     /** @deprecated A legacy feature for browser compatibility */
-//     compile(pattern: string, flags?: string): this;
-// }
+    // Non-standard extensions
+    /** @deprecated A legacy feature for browser compatibility */
+    compile(pattern: string, flags?: string): this;
+}
 
-// // TODO: $+
-// // interface RegExpConstructor {
-// //     new(pattern: RegExp | string): RegExp;
-// //     new(pattern: string, flags?: string): RegExp;
-// //     (pattern: RegExp | string): RegExp;
-// //     (pattern: string, flags?: string): RegExp;
-// //     readonly prototype: RegExp;
 
-// //     // Non-standard extensions
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $1: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $2: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $3: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $4: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $5: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $6: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $7: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $8: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $9: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     input: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     $_: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     lastMatch: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     "$&": string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     lastParen: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     "$+": string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     leftContext: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     "$`": string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     rightContext: string;
-// //     /** @deprecated A legacy feature for browser compatibility */
-// //     "$'": string;
-// // }
+interface RegExpConstructor {
+    new(pattern: RegExp | string): RegExp;
+    new(pattern: string, flags?: string): RegExp;
+    (pattern: RegExp | string): RegExp;
+    (pattern: string, flags?: string): RegExp;
+    readonly prototype: RegExp;
+
+    // Non-standard extensions
+    /** @deprecated A legacy feature for browser compatibility */
+    $1: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    $2: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    $3: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    $4: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    $5: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    $6: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    $7: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    $8: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    $9: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    input: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    $_: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    lastMatch: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    "$&": string;
+    /** @deprecated A legacy feature for browser compatibility */
+    lastParen: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    "$+": string;
+    /** @deprecated A legacy feature for browser compatibility */
+    leftContext: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    "$`": string;
+    /** @deprecated A legacy feature for browser compatibility */
+    rightContext: string;
+    /** @deprecated A legacy feature for browser compatibility */
+    "$'": string;
+}
 
 // // declare var RegExp: RegExpConstructor;
 
