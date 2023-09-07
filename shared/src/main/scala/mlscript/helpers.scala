36245bdd699740fc5720e8c59912f0b8f64a283f
NeilKleistGao <neilkleist@gmail.com>
2023-09-07 16:46:38 +0800

    Use is

--- shared/src/main/scala/mlscript/helpers.scala
+++ shared/src/main/scala/mlscript/helpers.scala
@@ -425,7 +425,7 @@ trait NuDeclImpl extends Located { self: NuDecl =>
           if (parents.isEmpty) "" else if (k === Als) " = " else ": "}${parents.mkString(", ")}"
   }
   lazy val genUnapply: Opt[NuFunDef] = this match {
-    case td: NuTypeDef if td.kind === Cls => td.params.fold[Opt[NuFunDef]](N)(tup => {
+    case td: NuTypeDef if td.kind is Cls => td.params.fold[Opt[NuFunDef]](N)(tup => {
       val ret = Tup(
         tup.fields.map {
           case S(p) -> f => N -> Fld(f.mut, f.spec, f.genGetter, Sel(Var("ins"), p))
