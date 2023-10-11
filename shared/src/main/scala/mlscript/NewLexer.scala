ed08423d61931f93c2fd7e8e4e2d3509c6d05718
HarrisL2 <harris.yh.lau@gmail.com>
2023-10-11 12:30:46 +0800

    Fix unary minus behaviour (#188)

--- shared/src/main/scala/mlscript/NewLexer.scala
+++ shared/src/main/scala/mlscript/NewLexer.scala
@@ -168,19 +168,14 @@ class NewLexer(origin: Origin, raise: Diagnostic => Unit, dbg: Bool) {
         // go(j, if (keywords.contains(n)) KEYWORD(n) else IDENT(n, isAlphaOp(n)))
         lex(j, ind, next(j, if (keywords.contains(n)) KEYWORD(n) else IDENT(n, isAlphaOp(n))))
       case _ if isOpChar(c) =>
-        if (c === '-' && isDigit(bytes(i + 1))) {
-          val (str, j) = takeWhile(i + 1)(isDigit)
-          lex(j, ind, next(j, LITVAL(IntLit(-BigInt(str)))))
-        } else {
-          val (n, j) = takeWhile(i)(isOpChar)
-          if (n === "." && j < length && isIdentFirstChar(bytes(j))) {
-            val (name, k) = takeWhile(j)(isIdentChar)
-            // go(k, SELECT(name))
-            lex(k, ind, next(k, SELECT(name)))
-          }
-          // else go(j, if (isSymKeyword.contains(n)) KEYWORD(n) else IDENT(n, true))
-          else lex(j, ind, next(j, if (isSymKeyword.contains(n)) KEYWORD(n) else IDENT(n, true)))
+        val (n, j) = takeWhile(i)(isOpChar)
+        if (n === "." && j < length && isIdentFirstChar(bytes(j))) {
+          val (name, k) = takeWhile(j)(isIdentChar)
+          // go(k, SELECT(name))
+          lex(k, ind, next(k, SELECT(name)))
         }
+        // else go(j, if (isSymKeyword.contains(n)) KEYWORD(n) else IDENT(n, true))
+        else lex(j, ind, next(j, if (isSymKeyword.contains(n)) KEYWORD(n) else IDENT(n, true)))
       case _ if isDigit(c) =>
         val (str, j) = takeWhile(i)(isDigit)
         // go(j, LITVAL(IntLit(BigInt(str))))
