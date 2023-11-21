e38416b21234f294b7b321214b7b12aa2f536dae
Luyu Cheng <chengluyu@live.cn>
2023-11-22 01:00:24 +0800

    Update the code that treats `Array` as a supertype of tuple types

--- shared/src/main/scala/mlscript/TyperHelpers.scala
+++ shared/src/main/scala/mlscript/TyperHelpers.scala
@@ -604,14 +604,19 @@ abstract class TyperHelpers { Typer: Typer =>
       case t @ RecordType(fs) => RecordType(fs.filter(nt => !names(nt._1)))(t.prov)
       case t @ TupleType(fs) =>
         val relevantNames = names.filter(n =>
-          n.name.startsWith("_")
-            && {
-              val t = n.name.tail
-              t.forall(_.isDigit) && {
-                val n = t.toInt
-                1 <= n && n <= fs.length
-              }
-            })
+          !n.name.isEmpty && n.name.forall(_.isDigit) && {
+            val index = n.name.toInt
+            0 <= index && index < fs.length
+          })
+          // // With old tuple field names:
+          // n.name.startsWith("_")
+          //   && {
+          //     val t = n.name.tail
+          //     t.forall(_.isDigit) && {
+          //       val n = t.toInt
+          //       1 <= n && n <= fs.length
+          //     }
+          //   })
         if (relevantNames.isEmpty) t
         else {
           val rcd = t.toRecord
