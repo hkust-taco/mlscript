99c9dc3ff911a404d4dac0fa8b42b796dc58f18f
waterlens <waterlens@waterlens.moe>
2024-01-17 21:03:01 +0800

    Fix things after merging

--- compiler/shared/main/scala/mlscript/compiler/optimizer/GraphIR.scala
+++ compiler/shared/main/scala/mlscript/compiler/optimizer/GraphIR.scala
@@ -236,7 +236,10 @@ enum GOExpr:
   
   def toDocument: Document = this match
     case Ref(s) => s.toString |> raw
-    case Literal(lit) => s"$lit" |> raw
+    case Literal(IntLit(lit)) => s"$lit" |> raw
+    case Literal(DecLit(lit)) => s"$lit" |> raw
+    case Literal(StrLit(lit)) => s"$lit" |> raw
+    case Literal(UnitLit(lit)) => s"$lit" |> raw
     case CtorApp(ClassInfo(_, name, _), args) =>
       raw(name) <#> raw("(") <#> raw(args |> show_args) <#> raw(")")
     case Select(s, _, fld) =>
