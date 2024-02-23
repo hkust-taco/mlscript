7d55167b0b0a81aa6e7148a73801073c8d636cb0
Lionel Parreaux <lionel.parreaux@gmail.com>
2024-02-23 12:45:20 +0800

    W DIFF

--- shared/src/main/scala/mlscript/Typer.scala
+++ shared/src/main/scala/mlscript/Typer.scala
@@ -291,16 +291,16 @@ class Typer(var dbg: Boolean, var verbose: Bool, var explainErrors: Bool, val ne
   
   private val preludeLoc = Loc(0, 0, Origin("<prelude>", 0, new FastParseHelpers("")))
   
-  // freshVar(noTyProv, N)(1)
-  // freshVar(noTyProv, N)(1)
-  // freshVar(noTyProv, N)(1)
-  // freshVar(noTyProv, N)(1)
-  // freshVar(noTyProv, N)(1)
-  // freshVar(noTyProv, N)(1)
-  // freshVar(noTyProv, N)(1)
-  // freshVar(noTyProv, N)(1)
-  // freshVar(noTyProv, N)(1)
-  // freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
+  freshVar(noTyProv, N)(1)
   
   val nuBuiltinTypes: Ls[NuTypeDef] = Ls(
     NuTypeDef(Cls, TN("Object"), Nil, N, N, N, Nil, N, N, TypingUnit(Nil))(N, S(preludeLoc), Nil),
