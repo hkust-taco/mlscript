3630433cb4cae52db1e97050d6e7526e10b9ef76
NeilKleistGao <neilkleist@gmail.com>
2024-06-03 12:09:05 +0800

    WIP: Add types

--- hkmc2/shared/src/main/scala/hkmc2/bbml/bbML.scala
+++ hkmc2/shared/src/main/scala/hkmc2/bbml/bbML.scala
@@ -13,8 +13,32 @@ sealed abstract class TypeArg
 case class Wildcard(in: Type, out: Type) extends TypeArg
 
 enum Type extends TypeArg:
-  case ClassType(targs: Ls[TypeArg])
+  case ClassType(name: ClassSymbol, targs: Ls[TypeArg])
   case InfVar(lvl: Int, uid: Uid[InfVar], state: VarState)
+  case FunType(args: Ls[Type], ret: Type, eff: Type)
+  case ComposedType(lhs: Type, rhs: Type, pol: Bool) // * positive -> union
+  case NegType(ty: Type)
+  case PolymorphicType(lvl: Int, tv: Ls[InfVar], body: Type)
+  case Top // TODO: level?
+  case Bot
+
+// TODO: apply
+opaque type RefType = Type.ClassType
+object RefType
+
+opaque type RegionType = Type.ClassType
+object RegionType
+
+opaque type CodeBaseType = Type.ClassType
+object CodeBaseType
+
+opaque type CodeType = CodeBaseType
+object CodeType
+
+opaque type VarType = CodeBaseType
+object VarType
+
+// TODO: builtin functions
 
 class VarState:
   var lowerBounds: Ls[Type] = Nil
