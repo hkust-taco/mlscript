75a5f55d233cc399a997bf54af146f55bd924701
NeilKleistGao <neilkleist@gmail.com>
2024-08-03 14:36:02 +0800

    Refactor collectBounds

--- hkmc2/shared/src/main/scala/hkmc2/bbml/PrettyPrinter.scala
+++ hkmc2/shared/src/main/scala/hkmc2/bbml/PrettyPrinter.scala
@@ -1,10 +1,11 @@
 package hkmc2
 package bbml
+import scala.collection.mutable.{Set => MutSet, ListBuffer}
 
 class PrettyPrinter(output: String => Unit):
   def print(ty: GeneralType): Unit =
     output(s"Type: ${ty}")
-    val bounds = PrettyPrinter.collectBounds(ty)(using Set.empty).distinct
+    val bounds = PrettyPrinter.collectBounds(ty).distinct
     if !bounds.isEmpty then
       output("Where:")
       bounds.foreach {
@@ -15,20 +16,19 @@ object PrettyPrinter:
   def apply(output: String => Unit): PrettyPrinter = new PrettyPrinter(output)
 
   type Bound = (Type, Type) // * Type <: Type
-  type TVCache = Set[Uid[InfVar]]
 
-  private def collectBounds(ty: GeneralType)(using cache: TVCache): List[Bound] = ty match
-    case ClassType(_, targs) => targs.flatMap {
-      case Wildcard(in, out) => collectBounds(in) ++ collectBounds(out)
-      case ty: Type => collectBounds(ty)
-    }
-    case v @ InfVar(_, uid, state, _) if !cache(uid) =>
-      val newCache = cache + uid
-      given TVCache = newCache
-      state.lowerBounds.flatMap(bd => (bd, v) :: collectBounds(bd)) ++ state.upperBounds.flatMap(bd => (v, bd) :: collectBounds(bd))
-    case FunType(args, ret, eff) => args.flatMap(collectBounds) ++ collectBounds(ret) ++ collectBounds(eff)
-    case PolyFunType(args, ret, eff) => args.flatMap(collectBounds) ++ collectBounds(ret) ++ collectBounds(eff)
-    case ComposedType(lhs, rhs, pol) => collectBounds(lhs) ++ collectBounds(rhs)
-    case NegType(ty) => collectBounds(ty)
-    case PolyType(tvs, body) => tvs.flatMap(collectBounds) ++ collectBounds(body)
-    case _ => Nil
+  private def collectBounds(ty: GeneralType): List[Bound] =
+    val res = ListBuffer[Bound]()
+    val cache = MutSet[Uid[InfVar]]()
+    object CollectBounds extends TypeTraverser:
+      override def apply(pol: Boolean)(ty: GeneralType): Unit = ty match
+        case v @ InfVar(_, uid, state, _) if !cache(uid) =>
+          cache += uid
+          res ++= state.lowerBounds.map(bd => (bd, v))
+          res ++= state.upperBounds.map(bd => (v, bd))
+          super.apply(pol)(ty)
+          cache -= uid
+        case _: InfVar => ()
+        case _ => super.apply(pol)(ty)
+    CollectBounds(true)(ty)
+    res.toList
