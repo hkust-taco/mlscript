5dc38ac751d4481f594a64221b2311299630e7b3
Luyu Cheng <chengluyu@live.cn>
2024-11-06 10:54:43 +0800

    Fix a typo

--- hkmc2/jvm/src/test/scala/hkmc2/DiffTestRunner.scala
+++ hkmc2/jvm/src/test/scala/hkmc2/DiffTestRunner.scala
@@ -82,7 +82,7 @@ abstract class DiffTestRunner(state: DiffTestRunner.State)
         "\n\tNote: you can increase this limit by changing DiffTests.TimeLimit")
       // * Thread.stop() is considered bad practice because normally it's better to implement proper logic
       // * to terminate threads gracefully, avoiding leaving applications in a bad state.
-      // * But here we DGAF since all the test is doing is runnign a type checker and some Node REPL,
+      // * But here we DGAF since all the test is doing is running a type checker and some Node REPL,
       // * which would be a much bigger pain to make receptive to "gentle" interruption.
       // * It would feel extremely wrong to intersperse the pure type checker algorithms
       // * with ugly `Thread.isInterrupted` checks everywhere...
