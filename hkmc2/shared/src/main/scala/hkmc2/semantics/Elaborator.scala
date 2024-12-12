ae7bfd50d3051af09db48c3322754dd6d952f6bc
NeilKleistGao <neilkleist@gmail.com>
2024-12-12 09:39:38 +0800

    Remove top-level getter selection check

--- hkmc2/shared/src/main/scala/hkmc2/semantics/Elaborator.scala
+++ hkmc2/shared/src/main/scala/hkmc2/semantics/Elaborator.scala
@@ -159,26 +159,18 @@ extends Importer:
     LetDecl(sym) :: DefineVar(sym, rhs) :: Nil
   
   def resolveField(srcTree: Tree, base: Opt[Symbol], nme: Ident)(using Ctx): Opt[FieldSymbol] =
+    def getFromModule(tree: Tree.TypeDef): Opt[FieldSymbol] = tree.definedSymbols.get(nme.name) match
+      case s @ S(clsSym) => s
+      case N =>
+        raise(ErrorReport(msg"Module '${tree.symbol.nme}' does not contain member '${nme.name}'" -> srcTree.toLoc :: Nil))
+        N
     base match
     case S(psym: BlockMemberSymbol) =>
       psym.modTree match
-      case S(cls) =>
-        cls.definedSymbols.get(nme.name) match
-        case s @ S(clsSym) => s
-        case N =>
-          raise(ErrorReport(msg"Module '${cls.symbol.nme}' does not contain member '${nme.name}'" -> srcTree.toLoc :: Nil))
-          N
+      case S(cls) => getFromModule(cls)
       case N =>
         N
-    case S(_: TopLevelSymbol) =>
-      def rec(ctx: Ctx): Opt[Ctx] = // * To avoid shadowing if users write `globalThis.somefield`
-        if ctx.getOuter.filter(_.isInstanceOf[TopLevelSymbol]).nonEmpty then S(ctx)
-        else ctx.parent match
-          case S(p) => rec(p)
-          case N => N
-      rec(ctx).flatMap(_.get(nme.name)).flatMap(_.symbol) match
-        case S(sym: FieldSymbol) => S(sym)
-        case _ => N
+    case S(msym: ModuleSymbol) => getFromModule(msym.tree)
     case _ => N
   
   def cls(tree: Tree, inAppPrefix: Bool): Ctxl[Term] = trace[Term](s"Elab class ${tree.showDbg}", r => s"~> $r"):
