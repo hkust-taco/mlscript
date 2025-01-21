5a09b0fb448fc20c47a004d790dbd5796d1c501a
Luyu Cheng <chengluyu@live.cn>
2025-01-21 22:58:43 +0800

    Rerun all tests

--- hkmc2/shared/src/test/mlscript-compile/Predef.mjs
+++ hkmc2/shared/src/test/mlscript-compile/Predef.mjs
@@ -157,7 +157,7 @@ const Predef$class = class Predef {
       len = rest.length;
       tmp4: while (true) {
         scrut = i2 < len;
-        if (scrut) {
+        if (scrut === true) {
           tmp = rest.at(i2) ?? null;
           tmp1 = f9(init, tmp) ?? null;
           init = tmp1;
@@ -178,7 +178,7 @@ const Predef$class = class Predef {
       let len, i2, init, scrut, scrut1, tmp, tmp1, tmp2, tmp3, tmp4, tmp5;
       len = rest.length;
       scrut1 = len == 0;
-      if (scrut1) {
+      if (scrut1 === true) {
         return first;
       } else {
         tmp = len - 1;
@@ -187,7 +187,7 @@ const Predef$class = class Predef {
         init = tmp1;
         tmp6: while (true) {
           scrut = i2 > 0;
-          if (scrut) {
+          if (scrut === true) {
             tmp2 = i2 - 1;
             i2 = tmp2;
             tmp3 = rest.at(i2) ?? null;
@@ -231,13 +231,13 @@ const Predef$class = class Predef {
       tmp5 = this.fold((arg1, arg2) => {
         return arg1 + arg2;
       });
-      if (isUB) {
+      if (isUB === true) {
         tmp6 = "";
       } else {
         tmp6 = "at least ";
       }
       scrut2 = expected === 1;
-      if (scrut2) {
+      if (scrut2 === true) {
         tmp7 = "";
       } else {
         tmp7 = "s";
