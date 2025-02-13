5099394f464d93a94d61033ca790cd9dbe425238
CAG2Mark <git@markng.com>
2025-02-13 13:36:01 +0800

    Update

--- hkmc2/shared/src/main/scala/hkmc2/codegen/StackSafeTransform.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/StackSafeTransform.scala
@@ -147,7 +147,7 @@ class StackSafeTransform(depthLimit: Int)(using State):
     val ClsLikeDefn(owner, isym, sym, k, paramsOpt, auxParams,
       parentPath, methods, privateFields, publicFields, preCtor, ctor) = defn
     ClsLikeDefn(
-      owner, isym, sym, k, paramsOpt, parentPath, methods.map(rewriteFn), privateFields,
+      owner, isym, sym, k, paramsOpt, auxParams, parentPath, methods.map(rewriteFn), privateFields,
       publicFields, rewriteBlk(preCtor),
       if isTopLevel && (defn.k is syntax.Mod) then transformTopLevel(ctor) else rewriteBlk(ctor)
     )
