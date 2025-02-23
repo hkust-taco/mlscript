09996d80ac3c3862faafb7c3cbbe2d123b814a0a
CAG2Mark <git@markng.com>
2025-02-23 15:57:30 +0800

    move lambda rewriter to new class

---
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/LambdaRewriter.scala
@@ -0,0 +1,33 @@
+package hkmc2
+
+import mlscript.utils.*, shorthands.*
+import utils.*
+
+import hkmc2.codegen.*
+import hkmc2.semantics.*
+import semantics.Elaborator.State
+
+class LambdaRewriter(using State):
+  def desugar(b: Block) =
+    def rewriteOneBlk(b: Block) =
+      var lambdasList: List[(BlockMemberSymbol, Value.Lam)] = Nil
+      val lambdaRewriter = new BlockTransformerNoRec(SymbolSubst()):
+        override def applyValue(v: Value): Value = v match
+          case lam: Value.Lam => 
+            val sym = BlockMemberSymbol("lambda", Nil)
+            lambdasList ::= (sym -> super.applyLam(lam))
+            Value.Ref(sym)
+          case _ => super.applyValue(v)
+      val blk = lambdaRewriter.applyBlock(b)
+      (blk, lambdasList)
+
+    val transformer = new BlockTransformer(SymbolSubst()):
+      override def applyBlock(b: Block): Block =
+        val (newBlk, lambdasList) = rewriteOneBlk(b)
+        val lambdaDefns = lambdasList.map:
+          case (sym, Value.Lam(params, body)) =>
+            FunDefn(None, sym, params :: Nil, body)
+        val ret = lambdaDefns.foldLeft(newBlk):
+          case (acc, defn) => Define(defn, acc)
+        super.applyBlock(ret)
+    transformer.applyBlock(b)
--- hkmc2/shared/src/main/scala/hkmc2/codegen/Lifter.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/Lifter.scala
@@ -959,35 +959,11 @@ class Lifter(using State, Raise):
 
   end liftDefnsInFn
 
-  def desugarLambdas(b: Block) =
-    def rewriteOneBlk(b: Block) =
-      var lambdasList: List[(BlockMemberSymbol, Value.Lam)] = Nil
-      val lambdaRewriter = new BlockTransformerNoRec(SymbolSubst()):
-        override def applyValue(v: Value): Value = v match
-          case lam: Value.Lam => 
-            val sym = BlockMemberSymbol("lambda", Nil)
-            lambdasList ::= (sym -> super.applyLam(lam))
-            Value.Ref(sym)
-          case _ => super.applyValue(v)
-      val blk = lambdaRewriter.applyBlock(b)
-      (blk, lambdasList)
-
-    val transformer = new BlockTransformer(SymbolSubst()):
-      override def applyBlock(b: Block): Block =
-        val (newBlk, lambdasList) = rewriteOneBlk(b)
-        val lambdaDefns = lambdasList.map:
-          case (sym, Value.Lam(params, body)) =>
-            FunDefn(None, sym, params :: Nil, body)
-        val ret = lambdaDefns.foldLeft(newBlk):
-          case (acc, defn) => Define(defn, acc)
-        super.applyBlock(ret)
-    transformer.applyBlock(b)
-
   // top-level
   def transform(b: Block) =
     // this is already done once in the lowering, but the handler lowering adds lambdas currently
     // so we need to desugar them again
-    val blk = desugarLambdas(b)
+    val blk = new LambdaRewriter().desugar(b)
 
     val analyzer = UsedVarAnalyzer(blk)
     val ctx = LifterCtx
