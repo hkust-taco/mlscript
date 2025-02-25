7edb5ba26d807b0d4fa27b247f19cb109a18cef1
CAG2Mark <git@markng.com>
2025-02-25 19:44:50 +0800

    preserve original lambda semantics

--- hkmc2/shared/src/main/scala/hkmc2/codegen/LambdaRewriter.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/LambdaRewriter.scala
@@ -9,24 +9,32 @@ import semantics.Elaborator.State
 
 object LambdaRewriter:
   def desugar(b: Block)(using State) =
-    def rewriteOneBlk(b: Block) =
-      var lambdasList: List[(BlockMemberSymbol, Value.Lam)] = Nil
-      val lambdaRewriter = new BlockDataTransformer(SymbolSubst()):
-        override def applyValue(v: Value): Value = v match
-          case lam: Value.Lam => 
-            val sym = BlockMemberSymbol("lambda", Nil, true)
-            lambdasList ::= (sym -> super.applyLam(lam))
-            Value.Ref(sym)
-          case _ => super.applyValue(v)
-      val blk = lambdaRewriter.applyBlock(b)
-      (blk, lambdasList)
+    def rewriteOneBlk(b: Block) = b match
+      case Assign(lhs, Value.Lam(params, body), rest) if !lhs.isInstanceOf[TempSymbol] =>
+        val newSym = BlockMemberSymbol(lhs.nme, Nil, false)
+        val blk = blockBuilder
+          .define(FunDefn(N, newSym, params :: Nil, body))
+          .assign(lhs, newSym.asPath)
+          .rest(rest)
+        (blk, Nil)
+      case _ =>
+        var lambdasList: List[(BlockMemberSymbol, Value.Lam)] = Nil
+        val lambdaRewriter = new BlockDataTransformer(SymbolSubst()):
+          override def applyValue(v: Value): Value = v match
+            case lam: Value.Lam => 
+              val sym = BlockMemberSymbol("lambda", Nil, true)
+              lambdasList ::= (sym -> super.applyLam(lam))
+              Value.Ref(sym)
+            case _ => super.applyValue(v)
+        val blk = lambdaRewriter.applyBlock(b)
+        (blk, lambdasList)
 
     val transformer = new BlockTransformer(SymbolSubst()):
       override def applyBlock(b: Block): Block =
         val (newBlk, lambdasList) = rewriteOneBlk(b)
         val lambdaDefns = lambdasList.map:
           case (sym, Value.Lam(params, body)) =>
-            FunDefn(None, sym, params :: Nil, body)
+            FunDefn(N, sym, params :: Nil, body)
         val ret = lambdaDefns.foldLeft(newBlk):
           case (acc, defn) => Define(defn, acc)
         super.applyBlock(ret)
