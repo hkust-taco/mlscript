af12459ad4aee2b1189298750099d9024af3425e
Anson Yeung <nanolify@gmail.com>
2025-03-10 22:42:04 +0800

    Remove obselete stuffs

--- hkmc2/shared/src/main/scala/hkmc2/codegen/HandlerLowering.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/HandlerLowering.scala
@@ -35,11 +35,8 @@ object HandlerLowering:
   // isTopLevel:
   // whether the current block is the top level block, as we do not emit code for continuation class on the top level
   // since we cannot return an effect signature on the top level (we are not in a function so return statement are invalid)
-  // and we do not have any `return` statement in the top level block so we do not need the `runtime.Return` workarounds.
   // contName: the name of the continuation class
   // ctorThis: the path to `this` in the constructor, this is used to insert `return this;` at the end of constructor.
-  // inScopeLocals: Local variables that are in scope.
-  // prevLocalsFn: The function that gets the outer function's locals.
   // linkAndHandle:
   // a function that takes a LinkState and returns a block that links the continuation class and handles the effect
   // this is a convenience function which initializes the continuation class in function context or throw an error in top level
@@ -54,6 +51,8 @@ object HandlerLowering:
     def nestDebugScope(locals: Set[Local], localsFn: Path) = copy(debugInfo = DebugInfo(
       debugInfo.inScopeLocals ++ locals, S(localsFn)))
   
+  // inScopeLocals: Local variables that are in scope.
+  // prevLocalsFn: The function that gets the outer function's locals.
   private case class DebugInfo(
       inScopeLocals: Set[Local],
       prevLocalsFn: Opt[Path],
