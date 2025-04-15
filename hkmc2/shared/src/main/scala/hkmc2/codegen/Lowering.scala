1929ca13c28046e40489980692f8ca05c3d8ca89
Luyu Cheng <2239547+chengluyu@users.noreply.github.com>
2025-04-15 13:25:49 +0800

    No longer `safeCheck` functions from the runtime

--- hkmc2/shared/src/main/scala/hkmc2/codegen/Lowering.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/Lowering.scala
@@ -298,7 +298,16 @@ class Lowering()(using Config, TL, Raise, State, Ctx):
         End("error")
     case st.TyApp(f, ts) => term(f)(k) // * Type arguments are erased
     case st.App(f, arg) =>
-      val isMlsFun = f.symbol.fold(f.isInstanceOf[st.Lam]):
+      val isMlsFun = f.symbol.fold(
+        f.isInstanceOf[st.Lam] || {
+          @tailrec def base(t: Term): Term = t match
+            case SynthSel(s, _) => base(s)
+            case _ => t
+          base(f) match
+            case st.Ref(symbol) => symbol == summon[State].runtimeSymbol
+            case _ => false
+        }
+      ):
         case _: sem.BuiltinSymbol => true
         case sym: sem.BlockMemberSymbol =>
           sym.trmImplTree.fold(sym.clsTree.isDefined)(_.k is syntax.Fun)
