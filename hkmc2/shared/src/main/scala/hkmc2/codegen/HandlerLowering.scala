a739238349a2ffe664098e3dbc5079050d5bb526
CAG2Mark <git@markng.com>
2025-05-15 19:14:22 +0800

    remove useless class

--- hkmc2/shared/src/main/scala/hkmc2/codegen/HandlerLowering.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/HandlerLowering.scala
@@ -44,10 +44,6 @@ object HandlerLowering:
   private case class LinkState(res: Local, cls: Path, uid: StateId, doUnwind: Opt[LazyVal[Path]])
   
   type FnOrCls = Either[BlockMemberSymbol, MemberSymbol[? <: ClassLikeDef] & InnerSymbol]
-  case class DoUnwindInfo(
-    funOrCtor: FnOrCls,
-    sym: BlockMemberSymbol
-  )
   
   // isTopLevel:
   // whether the current block is the top level block, as we do not emit code for continuation class on the top level
