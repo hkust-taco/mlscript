e35e4c185100c806f732807caba798d9ac46bb98
Florent Ferrari <fferrari@connect.ust.hk>
2025-05-16 13:06:35 +0800

    fixed empty record pattern behaviour

--- hkmc2/shared/src/main/scala/hkmc2/codegen/Lowering.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/Lowering.scala
@@ -491,14 +491,17 @@ class Lowering()(using Config, TL, Raise, State, Ctx):
                   mkMatch(mkArgs(clsParams.iterator.zip(args).collect { case (s1, S(s2)) => (s1, s2) }.toList))
               case Pattern.Tuple(len, inf) => mkMatch(Case.Tup(len, inf) -> go(tail, topLevel = false))
               case Pattern.Record(entries) =>
-                entries.foldRight
-                  (go(tail, topLevel = false)) {
+                val objectSym = summon[Ctx].builtins.Object
+                mkMatch( // checking that we have an object
+                  Case.Cls(objectSym, Value.Ref(BuiltinSymbol(objectSym.nme, false, false, true, false))),
+                  entries.foldRight(go(tail, topLevel = false)) {
                     case ((fieldName, fieldSymbol), blk) =>
                       mkMatch(
-                        Case.Field(fieldName),
+                        Case.Field(fieldName, safe = true), // we have checked object type already
                         Assign(fieldSymbol, Select(sr, fieldName)(N), blk)
                       )
                   }
+                )
         case Split.Else(els) =>
           if k.isInstanceOf[TailOp] && isIf then term_nonTail(els)(k)
           else
