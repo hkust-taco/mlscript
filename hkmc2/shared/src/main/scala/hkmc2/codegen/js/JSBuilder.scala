ac858d6911b435469573e79f3c9d5e830952e38d
CAG2Mark <git@markng.com>
2025-08-04 18:28:25 +0800

    remove fishy line

--- hkmc2/shared/src/main/scala/hkmc2/codegen/js/JSBuilder.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/js/JSBuilder.scala
@@ -341,8 +341,7 @@ class JSBuilder(using TL, State, Ctx) extends CodeBuilder:
                   val pss = pss_.map(setupFunction(N, _, End())._1)
                   val paramsDoc = pss.foldLeft(doc"($ps)"):
                     case (doc, ps) => doc"${doc}(${ps})"
-                  val extraBrace = if paramsOpt.isDefined then "" else "()" // TODO: fishy
-                  val bod = braced(doc" # return new ${sym.nme}.class$extraBrace$paramsDoc;")
+                  val bod = braced(doc" # return new ${sym.nme}.class$paramsDoc;")
                   val funBod = pss.foldRight(bod):
                     case (psDoc, doc_) => doc"($psDoc) => $doc_"
                   val funBodRet = if pss.isEmpty then funBod else braced(doc" # return $funBod")
