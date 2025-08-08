616516e7e03f40607d2a65df06cb9ab5c56f30ce
Gokul Rajiv <gokul.rajiv99@gmail.com>
2025-08-08 15:34:38 +0800

    trailing spaces

--- hkmc2DiffTests/src/test/scala/hkmc2/LlirDiffMaker.scala
+++ hkmc2DiffTests/src/test/scala/hkmc2/LlirDiffMaker.scala
@@ -60,7 +60,7 @@ abstract class LlirDiffMaker extends BbmlDiffMaker:
       )
 
   override def processTerm(trm: Blk, inImport: Bool, statefulComments: Ls[String])(using Config, Raise): Unit = 
-    super.processTerm(trm, inImport, statefulComments)    
+    super.processTerm(trm, inImport, statefulComments)
     if llir.isSet then
       val low = ltl.givenIn:
         codegen.Lowering()
