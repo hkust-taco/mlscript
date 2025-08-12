8911e30482c653d8141a6c6d363fe882a3e5f1b4
Flandia <flandia_@outlook.com>
2025-08-12 21:53:21 +0800

    Update hkmc2/shared/src/main/scala/hkmc2/semantics/Elaborator.scala
    Co-authored-by: Lionel Parreaux <lionel.parreaux@gmail.com>

--- hkmc2/shared/src/main/scala/hkmc2/semantics/Elaborator.scala
+++ hkmc2/shared/src/main/scala/hkmc2/semantics/Elaborator.scala
@@ -1266,15 +1266,15 @@ extends Importer:
     else VarSymbol(id)
   
   def param(t: Tree, inUsing: Bool, inDataClass: Bool): Ctxl[Diagnostic \/ (Param, Opt[SpreadKind])] =
-    t.desugared.asParam(inUsing).map: pt =>
-      log(s"Elaborating ParamTree: ${pt}")
-      val ParamTree(flags, id, sign, spd, modifiers) = pt
-      val flg = flags.copy(isVal = flags.isVal || inDataClass)
-      val sym = VarSymbol(id)
-      val sig = sign.map(term(_))
-      val p = Param(flg, sym, sig, Modulefulness.ofSign(sig)(Mod in modifiers))
-      sym.decl = S(p)
-      (p, spd)
+    t.desugared.asParam(inUsing).map:
+      case pt @ ParamTree(flags, id, sign, spd, modifiers) =>
+        log(s"Elaborating ParamTree: ${pt}")
+        val flg = flags.copy(isVal = flags.isVal || inDataClass)
+        val sym = VarSymbol(id)
+        val sig = sign.map(term(_))
+        val p = Param(flg, sym, sig, Modulefulness.ofSign(sig)(Mod in modifiers))
+        sym.decl = S(p)
+        (p, spd)
   
   def funParams(t: Tree): Ctxl[(ParamList, Ctx)] =
     val ps_ctx = params(t, inDataClass = false)
