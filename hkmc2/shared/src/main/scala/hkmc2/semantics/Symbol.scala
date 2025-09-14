c9988e2c04a511ff170c46390c7d583185750167
Flandia Yingman <me@flandia.dev>
2025-09-14 17:28:24 +0800

    Add masterful comment back (with minor tweak)

--- hkmc2/shared/src/main/scala/hkmc2/semantics/Symbol.scala
+++ hkmc2/shared/src/main/scala/hkmc2/semantics/Symbol.scala
@@ -99,6 +99,14 @@ abstract class Symbol(using State) extends Located:
       case S(defn: TermDefinition) => S(defn.sym)
       case N => N
 
+  /** 
+   * Get the symbol as the "representative" symbol of a set of
+   * overloaded definitions, or the sole definition, if it is not
+   * overloaded. 
+   *
+   * We should consider the ordering terms > classes/objects/types >
+   * modules, for this purpose.
+   * */
   def asPrincipal =
     asCls orElse
     asObj orElse
