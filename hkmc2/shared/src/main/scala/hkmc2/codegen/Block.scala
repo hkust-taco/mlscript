1871ea29882314306dc95c4cf4ad6023ff131034
ychenfo <43136427+ychenfo@users.noreply.github.com>
2025-11-28 01:00:27 +0800

    wip: smart constructors..?

--- hkmc2/shared/src/main/scala/hkmc2/codegen/Block.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/Block.scala
@@ -320,6 +320,47 @@ case class AssignDynField(lhs: Path, fld: Path, arrayIdx: Bool, rhs: Result, res
 
 case class Define(defn: Defn, rest: Block) extends Block with ProductWithTail
 
+object Match:
+  def apply(scrut: Path, arms: Ls[Case -> Block], dflt: Opt[Block], rest: Block): Block = rest match
+    case Scoped(syms, body) => Scoped(syms, Match(scrut, arms, dflt, body))
+    case _ => new Match(scrut, arms, dflt, rest)
+object Label:
+  def apply(label: Local, loop: Bool, body: Block, rest: Block): Block = rest match
+    case Scoped(syms, rest) => Scoped(syms, Label(label, loop, body, rest))
+    case _ => new Label(label, loop, body, rest)
+object Scoped:
+  def apply(syms: collection.Set[Local], body: Block): Block = body match
+    case Scoped(syms2, body) =>
+      if syms2.isEmpty && syms.isEmpty then Scoped(Set.empty, body) else Scoped(syms ++ syms2, body)
+    case _ =>
+      if syms.isEmpty then body else new Scoped(syms, body)
+object Begin:
+  def apply(sub: Block, rest: Block): Block = (sub, rest) match
+    case (Scoped(symsSub, bodySub), Scoped(symsRest, bodyRest)) =>
+      Scoped(symsSub ++ symsRest, Begin(bodySub, bodyRest))
+    case (Scoped(symsSub, bodySub), _) => Scoped(symsSub, Begin(bodySub, rest))
+    case (_, Scoped(symsRest, bodyRest)) => Scoped(symsRest, Begin(sub, bodyRest))
+    case _ => new Begin(sub, rest)
+object TryBlock:
+  def apply(sub: Block, finallyDo: Block, rest: Block): Block = rest match
+    case Scoped(syms, body) => Scoped(syms, TryBlock(sub, finallyDo, body))
+    case _ => new TryBlock(sub, finallyDo, rest)
+object Assign:
+  def apply(lhs: Local, rhs: Result, rest: Block): Block = rest match
+    case Scoped(syms, body) => Scoped(syms, Assign(lhs, rhs, body))
+    case _ => new Assign(lhs, rhs, rest)
+object AssignField:
+  def apply(lhs: Path, nme: Tree.Ident, rhs: Result, rest: Block)(symbol: Opt[MemberSymbol]): Block = rest match
+    case Scoped(syms, body) => Scoped(syms, AssignField(lhs, nme, rhs, body)(symbol))
+    case _ => new AssignField(lhs, nme, rhs, rest)(symbol)
+object AssignDynField:
+  def apply(lhs: Path, fld: Path, arrayIdx: Bool, rhs: Result, rest: Block): Block = rest match
+    case Scoped(syms, body) => Scoped(syms, AssignDynField(lhs, fld, arrayIdx, rhs, body))
+    case _ => new AssignDynField(lhs, fld, arrayIdx, rhs, rest)
+object Define:
+  def apply(defn: Defn, rest: Block): Block = rest match
+    case Scoped(syms, body) => Scoped(syms, Define(defn, body))
+    case _ => new Define(defn, rest)
 
 case class HandleBlock(
     lhs: Local,
@@ -332,6 +373,41 @@ case class HandleBlock(
     rest: Block
 ) extends Block with ProductWithTail
 
+
+object HandleBlock:
+  def apply(
+    lhs: Local,
+    res: Local,
+    par: Path,
+    args: Ls[Path],
+    cls: ClassSymbol,
+    handlers: Ls[Handler],
+    body: Block,
+    rest: Block
+  ) = rest match
+      case Scoped(syms, rest) =>
+        Scoped(
+          syms,
+          new HandleBlock(
+            lhs,
+            res,
+            par,
+            args,
+            cls,
+            handlers,
+            body,
+            rest
+          ))
+      case _ => new HandleBlock(
+        lhs,
+        res,
+        par,
+        args,
+        cls,
+        handlers,
+        body,
+        rest)
+    
 
 sealed abstract class Defn:
   val innerSym: Opt[MemberSymbol]
