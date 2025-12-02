4c7904e0adcda1375e875ce477ab7cc1a9002f57
ychenfo <43136427+ychenfo@users.noreply.github.com>
2025-12-02 13:17:00 +0800

    Revert "cleanup"
    This reverts commit 1948288e24f2b8af3bfbdf89cf95cb54d70f54bc.

--- hkmc2/shared/src/main/scala/hkmc2/semantics/Split.scala
+++ hkmc2/shared/src/main/scala/hkmc2/semantics/Split.scala
@@ -24,6 +24,7 @@ enum Split extends AutoLocated with ProductWithTail:
   case Else(default: Term)
   case End
   
+  
   inline def ~:(head: Branch): Split = Split.Cons(head, this)
   
   def mkClone(using State): Split = this match
