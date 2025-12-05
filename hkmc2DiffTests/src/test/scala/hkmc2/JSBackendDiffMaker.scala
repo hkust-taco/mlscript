9f23fed021d596052a4da05a364c2d2b84ce1881
NeilKleistGao <neilkleist@gmail.com>
2025-12-05 17:51:44 +0800

    Fix the missing case

--- hkmc2DiffTests/src/test/scala/hkmc2/JSBackendDiffMaker.scala
+++ hkmc2DiffTests/src/test/scala/hkmc2/JSBackendDiffMaker.scala
@@ -114,6 +114,7 @@ abstract class JSBackendDiffMaker extends MLsDiffMaker:
         case Return(res, implct) =>
           assert(implct)
           Assign(resSym, res, Return(Value.Lit(syntax.Tree.UnitLit(false)), true))
+        case _: Scoped => ??? // impossible. mapTail has handled this case specially 
         case tl: (Throw | Break | Continue) => tl
       )
       if showLoweredTree.isSet then
