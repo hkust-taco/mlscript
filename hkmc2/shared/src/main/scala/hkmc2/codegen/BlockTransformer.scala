31348be97d1761004c37403e6b801bfae36747a8
ychenfo <43136427+ychenfo@users.noreply.github.com>
2025-12-16 02:58:33 +0800

    simplify `LambdaRewriter`

--- hkmc2/shared/src/main/scala/hkmc2/codegen/BlockTransformer.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/BlockTransformer.scala
@@ -109,7 +109,7 @@ class BlockTransformer(subst: SymbolSubst):
   def applyScopedBlock(b: Block): Block = b match
     case Scoped(s, bd) =>
       val nb = applySubBlock(bd)
-      if nb is bd then b else new Scoped(s, nb)
+      if nb is bd then b else Scoped(s, nb)
     case _ => applySubBlock(b)
   
   
--- hkmc2/shared/src/main/scala/hkmc2/codegen/LambdaRewriter.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/LambdaRewriter.scala
@@ -11,60 +11,29 @@ import hkmc2.syntax.Tree
 object LambdaRewriter:
   
   def desugar(b: Block)(using State) =
-    
-    def rewriteOneBlk(b: Block) = b match
-      case Assign(lhs, Lambda(params, body), rest) if !lhs.isInstanceOf[TempSymbol] =>
-        val newSym = BlockMemberSymbol(lhs.nme, Nil,
-          nameIsMeaningful = true // TODO: lhs.nme is not always meaningful
-        )
-        val defn = FunDefn.withFreshSymbol(N, newSym, params :: Nil, body)(false)
-        val blk = blockBuilder
-          .define(defn)
-          .assign(lhs, defn.asPath)
-          .rest(rest)
-        (blk, Nil, List(newSym))
-      case _ =>
-        var lambdasList: List[((BlockMemberSymbol, TermSymbol), Lambda)] = Nil
-        var newLambdaSyms: List[BlockMemberSymbol] = Nil
-        val lambdaRewriter = new BlockDataTransformer(SymbolSubst()):
-          override def applyResult(r: Result)(k: Result => Block): Block = r match
-            case lam: Lambda => 
-              val sym = BlockMemberSymbol("lambda", Nil, nameIsMeaningful = false)
-              val tSym = TermSymbol.fromFunBms(sym, N)
-              lambdasList ::= ((sym, tSym) -> super.applyLam(lam))
-              newLambdaSyms ::= sym
-              k(Value.Ref(sym, S(tSym)))
-            case _ => super.applyResult(r)(k)
-        val blk = lambdaRewriter.applyBlock(b)
-        (blk, lambdasList, newLambdaSyms)
-    
     val transformer = new BlockTransformer(SymbolSubst()):
-      private var surroundingScopedSyms = Option.empty[collection.mutable.Set[Symbol]]
-      override def applyScopedBlock(b: Block): Block =
-        val prevsurroundingScopedSyms = surroundingScopedSyms
-        val res = b match
-          case Scoped(syms, body) => 
-            surroundingScopedSyms = Some(collection.mutable.Set.from(syms))
-            val newBody = applySubBlock(body)
-            if newBody is body then b
-            else new Scoped(surroundingScopedSyms.get, newBody)
-          case _ =>
-            surroundingScopedSyms = Some(collection.mutable.Set.empty[Symbol])
-            val newBlk = applySubBlock(b)
-            if newBlk is b then b
-            else new Scoped(surroundingScopedSyms.get, newBlk)
-        surroundingScopedSyms = prevsurroundingScopedSyms
-        res
+      override def applyResult(r: Result)(k: Result => Block): Block = r match
+        case lam: Lambda => 
+          val sym = BlockMemberSymbol("lambda", Nil, nameIsMeaningful = false)
+          val tSym = TermSymbol.fromFunBms(sym, N)
+          val lamDefn =
+            val Lambda(params, body) = super.applyLam(lam)
+            FunDefn(N, sym, tSym, params :: Nil, body)(false)
+          Scoped(Set(sym), Define(lamDefn, k(Value.Ref(sym, S(tSym)))))
+        case _ => super.applyResult(r)(k)
       
-      override def applyBlock(b: Block): Block =
-        val (newBlk, lambdasList, newLambdaSyms) = rewriteOneBlk(b)
-        val lambdaDefns = lambdasList.map:
-          case (sym, Lambda(params, body)) =>
-            FunDefn(N, sym._1, sym._2, params :: Nil, body)(false)
-        val ret = lambdaDefns.foldLeft(newBlk):
-          case (acc, defn) => Define(defn, acc)
-        surroundingScopedSyms.foreach(_.addAll(newLambdaSyms))
-        super.applyBlock(ret)
+      override def applyBlock(b: Block): Block = b match
+        case Assign(lhs, Lambda(params, body), rest) if !lhs.isInstanceOf[TempSymbol] =>
+          val newSym = BlockMemberSymbol(lhs.nme, Nil,
+            nameIsMeaningful = true // TODO: lhs.nme is not always meaningful
+          )
+          val defn = FunDefn.withFreshSymbol(N, newSym, params :: Nil, applyBlock(body))(false)
+          val blk = blockBuilder
+            .define(defn)
+            .assign(lhs, defn.asPath)
+            .rest(applyBlock(rest))
+          Scoped(Set(newSym), blk)
+        case _ => super.applyBlock(b)
     
     transformer.applyBlock(b)
   
