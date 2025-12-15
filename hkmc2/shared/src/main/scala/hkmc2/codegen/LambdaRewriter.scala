34faf7b5594aaa7f96df3e1f5a41360d73622550
ychenfo <43136427+ychenfo@users.noreply.github.com>
2025-12-15 20:07:12 +0800

    maintain scoped blocks in lambda rewriter

--- hkmc2/shared/src/main/scala/hkmc2/codegen/LambdaRewriter.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/LambdaRewriter.scala
@@ -22,28 +22,48 @@ object LambdaRewriter:
           .define(defn)
           .assign(lhs, defn.asPath)
           .rest(rest)
-        (blk, Nil)
+        (blk, Nil, List(newSym))
       case _ =>
         var lambdasList: List[((BlockMemberSymbol, TermSymbol), Lambda)] = Nil
+        var newLambdaSyms: List[BlockMemberSymbol] = Nil
         val lambdaRewriter = new BlockDataTransformer(SymbolSubst()):
           override def applyResult(r: Result)(k: Result => Block): Block = r match
             case lam: Lambda => 
               val sym = BlockMemberSymbol("lambda", Nil, nameIsMeaningful = false)
               val tSym = TermSymbol.fromFunBms(sym, N)
               lambdasList ::= ((sym, tSym) -> super.applyLam(lam))
+              newLambdaSyms ::= sym
               k(Value.Ref(sym, S(tSym)))
             case _ => super.applyResult(r)(k)
         val blk = lambdaRewriter.applyBlock(b)
-        (blk, lambdasList)
+        (blk, lambdasList, newLambdaSyms)
     
     val transformer = new BlockTransformer(SymbolSubst()):
+      private var surroundingScopedSyms = Option.empty[collection.mutable.Set[Symbol]]
+      override def applyScopedBlock(b: Block): Block =
+        val prevsurroundingScopedSyms = surroundingScopedSyms
+        val res = b match
+          case Scoped(syms, body) => 
+            surroundingScopedSyms = Some(collection.mutable.Set.from(syms))
+            val newBody = applySubBlock(body)
+            if newBody is body then b
+            else new Scoped(surroundingScopedSyms.get, newBody)
+          case _ =>
+            surroundingScopedSyms = Some(collection.mutable.Set.empty[Symbol])
+            val newBlk = applySubBlock(b)
+            if newBlk is b then b
+            else new Scoped(surroundingScopedSyms.get, newBlk)
+        surroundingScopedSyms = prevsurroundingScopedSyms
+        res
+      
       override def applyBlock(b: Block): Block =
-        val (newBlk, lambdasList) = rewriteOneBlk(b)
+        val (newBlk, lambdasList, newLambdaSyms) = rewriteOneBlk(b)
         val lambdaDefns = lambdasList.map:
           case (sym, Lambda(params, body)) =>
             FunDefn(N, sym._1, sym._2, params :: Nil, body)(false)
         val ret = lambdaDefns.foldLeft(newBlk):
           case (acc, defn) => Define(defn, acc)
+        surroundingScopedSyms.foreach(_.addAll(newLambdaSyms))
         super.applyBlock(ret)
     
     transformer.applyBlock(b)
