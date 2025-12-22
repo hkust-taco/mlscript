c26e6ee804a64f92757433d8a7644e951f15e2ba
Anson Yeung <nanolify@gmail.com>
2025-12-22 16:36:44 +0800

    Enable sanity checks again

--- hkmc2DiffTests/src/test/scala/hkmc2/MLsDiffMaker.scala
+++ hkmc2DiffTests/src/test/scala/hkmc2/MLsDiffMaker.scala
@@ -81,8 +81,8 @@ abstract class MLsDiffMaker extends DiffMaker:
     if !effectHandlers.get.forall(effectHandlersOptions.contains(_)) then
       output(s"$errMarker Option ':effectHandlers' only supports 'debug' as option")
     if effectHandlers.isSet then
-      if liftDefns.isUnset || noSanityCheck.isUnset then
-        output(s"$errMarker Option ':effectHandlers' requires ':lift' and ':noSanityCheck'")
+      if liftDefns.isUnset then
+        output(s"$errMarker Option ':effectHandlers' requires ':lift'")
     Config(
       sanityChecks = Opt.when(noSanityCheck.isUnset)(SanityChecks(light = true)),
       effectHandlers = Opt.when(effectHandlers.isSet)(EffectHandlers(
