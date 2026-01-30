16240f7b4aca9d9ba8766b7410014f5633ff13d2
NeilKleistGao <neilkleist@gmail.com>
2026-01-30 10:18:01 +0800

    Minor fix and clean

--- hkmc2/shared/src/main/scala/hkmc2/codegen/Defunctionalization.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/Defunctionalization.scala
@@ -5,25 +5,38 @@ import utils.*
 import semantics.*
 import syntax.Tree
 import semantics.Elaborator.{ctx, State}
+import hkmc2.Message.MessageContext
 
 import collection.mutable.HashMap
 
 
-class Defunctionalization(using Elaborator.State, Elaborator.Ctx) extends BlockTransformer(new SymbolSubst):
+class Defunctionalization(using Elaborator.State, Raise) extends BlockTransformer(new SymbolSubst):
 
+  // Collect functions in a single module and
+  //  1. generate corresponding function class with a call function
+  //  2. generate firstCls field for each non-anonymous function
+  //  3. substitute all first-class functions with corresponding class instantiation.
+  //    if the function is defined in another module, the class can be retrieved by firstCls field
+  //  4. invoke the call function for each first-class functions
   class DefunctionalizationInModule(outModulePath: Option[Path], mapping: HashMap[BlockMemberSymbol, FunDefn]) extends BlockTransformer(new SymbolSubst):
     private def callFunc(fd: FunDefn) =
       val f = outModulePath.map(_.selSN(fd.sym.nme)).getOrElse(fd.asPath)
-      val params = fd.params.head.params.map(p => Value.Ref(p.sym).asArg) // TODO: remove head
+      val params = fd.params match
+        case head :: Nil => head.params.map(p => Value.Ref(p.sym).asArg)
+        case _ =>
+          raise(ErrorReport(msg"Unsupported function form." -> fd.sym.toLoc :: Nil,
+            source = Diagnostic.Source.Compilation))
+          Nil
+      
       Return(Call(f, params)(true, false, false), false)
 
     override def applyBlock(b: Block): Block = b match
       case Define(defn, rst) => defn match
-        case fd @ FunDefn(owner, sym, dSym, params, body) if !sym.nameIsMeaningful => // lambda functions are here
+        case fd @ FunDefn(owner, sym, dSym, params, body) if !sym.nameIsMeaningful => // Anonymous functions
           val lamClsSym = new BlockMemberSymbol("Lambda$" + mapping.size.toString(), Nil, false)
           mapping += (sym -> FunDefn.withFreshSymbol(owner, lamClsSym, params, body)(fd.forceTailRec))
-          applyBlock(rst)
-        case fd @ FunDefn(owner, sym, dSym, params, body) =>
+          applyBlock(rst) // Also remove the original definition, since they cannot be invoked by name
+        case fd @ FunDefn(owner, sym, dSym, params, body) => // Non-anonymous functions
           val lamClsSym = new BlockMemberSymbol("Lambda$" + sym.nme + mapping.size.toString(), Nil, false)
           mapping += (sym -> FunDefn.withFreshSymbol(owner, lamClsSym, params, callFunc(fd))(fd.forceTailRec))
           applyDefn(fd): fd2 =>
@@ -34,8 +47,10 @@ class Defunctionalization(using Elaborator.State, Elaborator.Ctx) extends BlockT
         case _ => super.applyBlock(b)
       case _ => super.applyBlock(b)
 
+    // Pack module methods together to defunctionalize them
     private def packMethods(ms: List[FunDefn]) = ms.foldRight[Block](End())((fd, rst) => Define(fd, rst))
 
+    // Extract defunctionalized methods and corresponding firstCls assignments
     private def unpackMethods(b: Block, rest: Block) =
       def rec(b: Block, acc1: List[FunDefn], acc2: Block): (List[FunDefn], Block) = b match
         case Define(fd: FunDefn, rest) => rec(rest, fd :: acc1, acc2)
@@ -60,7 +75,8 @@ class Defunctionalization(using Elaborator.State, Elaborator.Ctx) extends BlockT
           case _ => super.applyDefn(defn)(k)
       case _ => super.applyDefn(defn)(k)
 
-    private def updateRHSPath(p: Path, mustBeAnonymous: Boolean)(k: Path => Block) = p match
+    // If the given path is non-anonymous and at LHS (i.e., mustBeAnonymous is true), then do not substitute it
+    private def updatePathWithInst(p: Path, mustBeAnonymous: Boolean)(k: Path => Block) = p match
       case ref @ Value.Ref(l: BlockMemberSymbol, disamb) if !l.nameIsMeaningful || !mustBeAnonymous => mapping.get(l) match
         case Some(fd) =>
           val tmp = new TempSymbol(None, "tmp")
@@ -68,7 +84,7 @@ class Defunctionalization(using Elaborator.State, Elaborator.Ctx) extends BlockT
           Scoped(Set(tmp),
             Assign(tmp,
               Instantiate(false, cls, fd.capturedVariables.map(v => Value.Ref(v, None).asArg)), k(Value.Ref(tmp, None))))
-        case None if l.trmTree.isDefined =>
+        case None if l.tsym.map(_.k == syntax.Fun).getOrElse(false) => // This symbol denotes a function and is defined in another module
           val tmp = new TempSymbol(None, "tmp")
           val cls = ref.selSN("firstCls")
           Scoped(Set(tmp),
@@ -76,16 +92,16 @@ class Defunctionalization(using Elaborator.State, Elaborator.Ctx) extends BlockT
               Instantiate(false, cls, Nil), k(Value.Ref(tmp, None))))
         case _ => k(p)
       case sel: Select => sel.symbol match
-        case Some(s: TermSymbol) =>
+        case Some(s: TermSymbol) if s.k == syntax.Fun =>
           val blkSym = mapping.find(p => p._1.tsym match
             case Some(t) => t == s
             case _ => false
           )
           blkSym match
-            case Some(p) =>
+            case Some(p) => // we are selecting a function inside the current module
               k(outModulePath.map(_.selSN(p._1.nme)).getOrElse(Value.Ref(p._1, None)))
             case _ => s.owner match
-              case Some(_: ModuleOrObjectSymbol) =>
+              case Some(_: ModuleOrObjectSymbol) => // defined in another module
                 val tmp = new TempSymbol(None, "tmp")
                 val cls = sel.selSN("firstCls")
                 Scoped(Set(tmp),
@@ -96,21 +112,27 @@ class Defunctionalization(using Elaborator.State, Elaborator.Ctx) extends BlockT
       case _ => k(p)
 
     override def applyRcdArg(rcdArg: RcdArg)(k: RcdArg => Block): Block =
-      updateRHSPath(rcdArg.value, false): v =>
+      updatePathWithInst(rcdArg.value, false): v =>
         super.applyRcdArg(RcdArg(rcdArg.idx, v))(k)
 
     override def applyArg(arg: Arg)(k: Arg => Block): Block =
-      updateRHSPath(arg.value, false): v =>
+      updatePathWithInst(arg.value, false): v =>
         super.applyArg(Arg(arg.spread, v))(k)
 
     override def applyResult(r: Result)(k: Result => Block): Block = r match
-      case c @ Call(fun, args) => updateRHSPath(fun, true): fun2 =>
+      case c @ Call(fun, args) => updatePathWithInst(fun, true): fun2 =>
         applyArgs(args): args2 =>
-          k(Call(fun2, args2)(c.isMlsFun, c.mayRaiseEffects, c.explicitTailCall))
-      case p: Path => updateRHSPath(p, false): p2 =>
+          fun2 match
+            case ref @ Value.Ref(sym, _) => sym match
+              case _: VarSymbol |  _: TempSymbol =>
+                k(Call(ref.selSN("call"), args2)(c.isMlsFun, c.mayRaiseEffects, c.explicitTailCall))
+              case _ => k(Call(fun2, args2)(c.isMlsFun, c.mayRaiseEffects, c.explicitTailCall))
+            case _ => k(Call(fun2, args2)(c.isMlsFun, c.mayRaiseEffects, c.explicitTailCall))
+      case p: Path => updatePathWithInst(p, false): p2 =>
         k(p2)
       case _ => super.applyResult(r)(k)
   
+  // Substitute captured symbols in anonymous lambda bodies with corresponding class fields
   class UpdateReference(using subst: Map[Symbol, Symbol]) extends BlockTransformer(new SymbolSubst):
     override def applyLocal(sym: Symbol): Symbol = subst.get(sym) match
       case Some(r) => r
@@ -122,17 +144,6 @@ class Defunctionalization(using Elaborator.State, Elaborator.Ctx) extends BlockT
         k(Value.Ref(l2, disamb))
       case _ => super.applyValue(v)(k)
 
-  class UpdateCall() extends BlockTransformer(new SymbolSubst):
-    override def applyResult(r: Result)(k: Result => Block): Block = r match
-      case r @ Call(fun, args) => fun match
-        case ref @ Value.Ref(sym, _) => sym match
-          case _: VarSymbol |  _: TempSymbol =>
-            k(Call(ref.selSN("call"), args)(r.isMlsFun, r.mayRaiseEffects, r.explicitTailCall))
-          case _ => super.applyResult(r)(k)
-        case _ => super.applyResult(r)(k)
-      case _ => super.applyResult(r)(k)
-    
-
   private def generateFCFunctionClasses(owner: Option[InnerSymbol], funcs: List[FunDefn], rest: Block): Block = funcs.foldRight(rest)(
     (func, res) =>
       val capturedVariables = func.capturedVariables
@@ -143,8 +154,7 @@ class Defunctionalization(using Elaborator.State, Elaborator.Ctx) extends BlockT
       val ctorParams = capturedVariables.map(v => new VarSymbol(v.id))
       val cvMems = capturedVariables.map(v => TermSymbol(syntax.MutVal, Some(clsSym), Tree.Ident(v.nme)))
       val ctor = ctorParams.zip(cvMems).foldRight[Block](End())((p, res) => Assign(p._2, Value.Ref(p._1), res))
-      val body =
-        new UpdateReference(using capturedVariables.zip(cvMems).toMap).applyBlock(func.body)
+      val body = new UpdateReference(using capturedVariables.zip(cvMems).toMap).applyBlock(func.body)
       val clsDef = ClsLikeDefn(owner, clsSym, func.sym, None, syntax.Cls,
         Some(PlainParamList(ctorParams.map(Param.simple))), Nil,
         Some(Value.Ref(State.globalThisSymbol).selSN("Function")),
@@ -157,8 +167,7 @@ class Defunctionalization(using Elaborator.State, Elaborator.Ctx) extends BlockT
     val fcfDefs = HashMap.empty[BlockMemberSymbol, FunDefn]
     val noFirstClassFunc = new DefunctionalizationInModule(None, fcfDefs).applyBlock(b)
     val fcfCls = fcfDefs.map(_._2).toList
-    val withClasses = generateFCFunctionClasses(None, fcfCls, noFirstClassFunc)
-    new UpdateCall().applyBlock(withClasses)
+    generateFCFunctionClasses(None, fcfCls, noFirstClassFunc)
 
   extension (fd: FunDefn) {
     def capturedVariables: List[VarSymbol | TermSymbol] =
