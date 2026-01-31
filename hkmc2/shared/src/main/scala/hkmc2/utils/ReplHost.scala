e7605bb7649d83fd2a820b55cbefa9618a97d710
FetBoba <fsam@connect.ust.hk>
2026-01-31 18:15:58 +0500

    Reverted string implementation

--- hkmc2/shared/src/main/scala/hkmc2/utils/ReplHost.scala
+++ hkmc2/shared/src/main/scala/hkmc2/utils/ReplHost.scala
@@ -11,9 +11,8 @@ import hkmc2.utils.*
 class ReplHost(rootPath: Str)(using TL) {
   
   private val builder = new java.lang.ProcessBuilder()
-  // `--interactive` always enters the REPL even if stdin is not a terminal.
-  // `--experimental-wasm-stringref` is required for Binaryen stringref opcodes like `string.const`.
-  builder.command("node", "--interactive", "--experimental-wasm-stringref")
+  // `--interactive` always enters the REPL even if stdin is not a terminal
+  builder.command("node", "--interactive")
   private val proc = builder.start()
 
   private val stdin = new BufferedWriter(new OutputStreamWriter(proc.getOutputStream))
