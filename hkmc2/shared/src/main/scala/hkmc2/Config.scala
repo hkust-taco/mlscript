734eabbc157a95357a1bf46c109d3eda2a0190b2
Anson Yeung <nanolify@gmail.com>
2026-02-25 01:03:31 +0800

    Update comment

--- hkmc2/shared/src/main/scala/hkmc2/Config.scala
+++ hkmc2/shared/src/main/scala/hkmc2/Config.scala
@@ -70,7 +70,7 @@ object Config:
     // A debug option that allows codegen to continue even if an unlifted definition is encountered.
     softLifterError: Bool = false,
     // Skips instrumenting module constructors, this can be used when the file is statically known to not
-    // raise any effect and cannot use runtime during module construction due to cyclic dependency.
+    // raise any effect and cannot use the Runtime.mls module during module construction due to cyclic dependency.
     doNotInstrumentTopLevelModCtor: Bool = false,
   )
   
