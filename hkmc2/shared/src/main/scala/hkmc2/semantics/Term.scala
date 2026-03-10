cfff853c02a5e7a354f6a1b70e834fe7f363916a
Maxime Mulder <maxime-mulder@outlook.com>
2026-03-10 17:39:31 +0800

    quick change docstring style before lionel sees it

--- hkmc2/shared/src/main/scala/hkmc2/semantics/Term.scala
+++ hkmc2/shared/src/main/scala/hkmc2/semantics/Term.scala
@@ -277,7 +277,7 @@ enum Term extends Statement:
   case UnitVal()
   case Missing // Placeholder terms that were not elaborated due to the "lightweight" elaboration mode `Mode.Light`
   /** A subtyping constraint. This term is mostly useful at the top-level for testing purposes and
-   *  is not meant to be used in a real program. */
+    * is not meant to be used in a real program. */
   case SubConstr(constraint: SubConstraint) extends Term
   case Lit(lit: Literal)
   /** A term that wraps another term, indicating that the symbol of the inner term is resolved.
