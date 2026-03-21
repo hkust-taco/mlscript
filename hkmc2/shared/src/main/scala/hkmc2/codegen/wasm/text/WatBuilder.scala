643d0ceb1d49965da19f800c6ad8fdb38b557018
FetBoba <fsam@connect.ust.hk>
2026-03-21 10:05:37 +0800

    Added compilation error for plain class refs

--- hkmc2/shared/src/main/scala/hkmc2/codegen/wasm/text/WatBuilder.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/wasm/text/WatBuilder.scala
@@ -556,9 +556,22 @@ class WatBuilder(using TraceLogger, State) extends CodeBuilder:
       singletonInfoFor(l) match
         case S(info) => singletonGlobalGet(info)
         case N =>
-          ctx.getFunc(l) match
-            case S(funcIdx) => ref.func(funcIdx, RefType(ctx.getFuncInfo_!(l).typeIdx, nullable = false))
-            case N => getVar(l, r.toLoc)
+          val isPlainClassRef = l match
+            case _: ClassSymbol => true
+            case _ =>
+              disamb.exists:
+                case _: ClassSymbol => true
+                case ds => ds.defn.exists:
+                    case _: ClassDef => true
+                    case _ => false
+          if isPlainClassRef then
+            errExpr(
+              Ls(msg"Plain class references are not supported in Wasm; instantiate the class instead." -> r.toLoc),
+            )
+          else
+            ctx.getFunc(l) match
+              case S(funcIdx) => ref.func(funcIdx, RefType(ctx.getFuncInfo_!(l).typeIdx, nullable = false))
+              case N => getVar(l, r.toLoc)
 
     case Call(Value.Ref(l: BuiltinSymbol, _), lhs :: rhs :: Nil) if !l.functionLike =>
       if l.binary then
