c14fc9ee9a53c28d77301a79aa77b9ddc8b87c2b
Luyu Cheng <2239547+chengluyu@users.noreply.github.com>
2026-04-03 17:51:54 +0800

    Address merge inconsistencies

--- hkmc2/shared/src/main/scala/hkmc2/codegen/Printer.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/Printer.scala
@@ -37,7 +37,6 @@ class Printer(using Raise, ShowCfg, SymbolPrinter):
         case Case.Cls(cls, path) => doc"${print(cls)}"
         case Case.Tup(len, inf) => doc"Array($len${if inf then "+" else ""})"
         case Case.Field(name, safe) => doc"${if safe then "" else "Object "}{ ${name.name} }"
-        case _ => TODO(c)
       val docCases = arms
         .map{ case (c, b) => doc"${case_doc(c)} => #{  # ${print(b)} #} " }
         .mkDocument(sep = doc" # ")
