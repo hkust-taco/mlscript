ecd0363a3bc64c50ab599a626d7737d197fd80ae
Anson Yeung <nanolify@gmail.com>
2026-04-03 21:08:56 +0800

    Fix typo

--- hkmc2/shared/src/main/scala/hkmc2/codegen/BlockSimplifier.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/BlockSimplifier.scala
@@ -275,7 +275,7 @@ class BlockSimplifier(symbolsToPreserve: Set[Local])(using DebugPrinter, State, 
         def isPrivate = !symbolsToPreserve.contains(defn.sym)
         
         // Whether this function can be inlined without causing any code duplication,
-        // i.e. the original defintion can be removed and there is only one usage.
+        // i.e. the original definition can be removed and there is only one usage.
         def canBeInlineEliminated =
           isPrivate && !isMethod && useCount <= 1 && !hasNakedRef && !isLoopBreaker
           // false
