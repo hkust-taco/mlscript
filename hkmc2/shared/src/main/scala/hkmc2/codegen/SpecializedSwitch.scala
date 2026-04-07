b7916de048177826b8ef2cb28850b1cbd26a77fb
CAG2Mark <git@markng.com>
2026-04-07 17:05:32 +0800

    working

--- hkmc2/shared/src/main/scala/hkmc2/codegen/SpecializedSwitch.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/SpecializedSwitch.scala
@@ -14,7 +14,7 @@ import scala.annotation.tailrec
   * @param l The case's literal value.
   * @param b The case body.
   */
-enum SwitchCase(l: Literal, b: Block):
+enum SwitchCase(val litValue: Literal, val body: Block):
   /**
     * A switch case that requires an explicit `break` to be inserted by the codegen.
     * @param l The case's literal value.
@@ -22,7 +22,7 @@ enum SwitchCase(l: Literal, b: Block):
     */
   case ExplicitBreak(l: Literal, b: Block) extends SwitchCase(l, b)
   /**
-    * A switch case that already has a `break` or `continue` at the end.
+    * A switch case that already has a `break`, `return` or `continue` at the end.
     * @param l The case's literal value.
     * @param b The case body.
     */
@@ -73,8 +73,9 @@ private enum MatchType:
 // N: None of the cases
 @tailrec
 private def caseLastBlk(b: Block, scrutSym: Local): Opt[Opt[Literal]] = b match
-  case a @ Assign(`scrutSym`, l: Literal, End(_)) => S(S(l))
+  case a @ Assign(`scrutSym`, Value.Lit(l), End(_)) => S(S(l))
   case b: (Break | Continue) => S(N)
+  case Return(_, false) => S(N)
   case b: NonBlockTail => caseLastBlk(b.rest, scrutSym)
   case _: BlockTail => N
 
@@ -83,41 +84,59 @@ private object LitCases:
     case (S(acc), Case.Lit(litVal) -> b) => S((litVal -> b) :: acc)
     case _ => N
 
+private case class MatchChain(scrut: Value.Ref, cases: List[MatchType], dflt: Opt[Block], rest: Block)
+
+private def isEmptyDflt(dflt: Opt[Block]) = dflt match
+  case Some(End(_)) => true
+  case None => true
+  case _ => false
+
+
 @tailrec
 private def findMatchChainRec(
   b: Block,
-  scrutSym: Local,
+  scrutRef: Value.Ref,
   acc: List[MatchType]
-): (cases: List[MatchType], dflt: Opt[Block], rest: Block) =
-  
+): MatchChain =
   object CaseLastBlk:
-    def unapply(b: Block) = caseLastBlk(b, scrutSym)
+    def unapply(b: Block) = caseLastBlk(b, scrutRef.l)
   
-  inline def join: (cases: List[MatchType], dflt: Opt[Block], rest: Block) = b match
+  // Allowed iff the previous case was a break, or if this is the only case
+  val isDfltCaseAllowed = acc.headOption match
+    case Some(_: MatchType.MBreak) => true
+    case None => true
+    case _ => false
+  
+  inline def fail = MatchChain(scrutRef, acc, N, b)
+  
+  inline def join: MatchChain = b match
     case m: Match =>
-      // Classify the current match statement.
-      val curMatch = b match
-        // MFallthrough or MBreak
-        case Match(
-          Value.Ref(`scrutSym`, _),                               // * The scrutinee is a ref and is the same as the one before.
-          Case.Lit(curVal) -> (b @ CaseLastBlk(nextVal)) :: Nil,  // * There is only one case matching an int literal
-                                                                  //   and it ends with break, continue or a literal assignment.
-          default, restBlk
-        ) => nextVal match
-          case S(nextVal) => S(MatchType.MFallthrough(curVal, b, nextVal))
-          case N => S(MatchType.MBreak(curVal, b))
-        // MCases
-        case Match(Value.Ref(`scrutSym`, _), LitCases(arms), default, restBlk) =>
-          S(MatchType.MCases(arms))
-        case _ => N
-      
-      curMatch match
-      case Some(value) =>
-        // Only the last match may have a default case.
-        if m.dflt.isDefined then (value :: acc, m.dflt, m.rest)
-        else findMatchChainRec(m.rest, scrutSym, value :: acc)
-      case None => (acc, N, m)
-    case _ => (acc, N, b)
+      val dfltEmpty = isEmptyDflt(m.dflt)
+      if !isDfltCaseAllowed && !dfltEmpty then fail
+      else
+        // Classify the current match statement.
+        val curMatch = m match
+          // MFallthrough or MBreak
+          case Match(
+            `scrutRef`,                                             // * The scrutinee is a ref and is the same as the one before.
+            Case.Lit(curVal) -> (b @ CaseLastBlk(nextVal)) :: Nil,  // * There is only one case matching an int literal
+                                                                    //   and it ends with break, continue, return, or a literal assignment.
+            default, restBlk
+          ) => nextVal match
+            case S(nextVal) => S(MatchType.MFallthrough(curVal, b, nextVal))
+            case N => S(MatchType.MBreak(curVal, b))
+          // MCases
+          case Match(`scrutRef`, LitCases(arms), default, restBlk) =>
+            S(MatchType.MCases(arms))
+          case _ => N
+        
+        curMatch match
+        case Some(value) =>
+          // Only the last match may have a default case.
+          if !dfltEmpty then MatchChain(scrutRef, value :: acc, m.dflt, m.rest)
+          else findMatchChainRec(m.rest, scrutRef, value :: acc)
+        case None => fail
+    case _ => fail
   
   
   val curVal = b match
@@ -126,12 +145,31 @@ private def findMatchChainRec(
     case _ => N
   
   acc.headOption match
-    case Some(MatchType.MFallthrough(next = expectedVal))
-      if curVal.map(_ == expectedVal).getOrElse(true) => join
-    case Some(_: MatchType.MBreak) | None => join
-    case _ => (acc, N, b)
+    case S(MatchType.MFallthrough(next = expectedVal))
+      if curVal.map(_ == expectedVal).getOrElse(true) =>
+        join
+    case S(_: MatchType.MBreak) | N => join
+    case S(_) => MatchChain(scrutRef, acc, N, b)
 
+private case class SwitchLikeBlock(scrut: Value.Ref, cases: List[SwitchCase], dflt: Opt[Block], rest: Block)
 
+def matchChainToSwitch(m: MatchChain): SwitchLikeBlock =
+  val cases = m.cases.flatMap:
+    case MatchType.MFallthrough(value, body, next) =>
+      SwitchCase.Fallthrough(value, body, next) :: Nil
+    case MatchType.MBreak(value, body) =>
+      SwitchCase.ImplicitBreak(value, body) :: Nil
+    case MatchType.MCases(arms) => arms.map:
+      case (l, b) => SwitchCase.ExplicitBreak(l, b)
+  SwitchLikeBlock(m.scrut, cases, m.dflt, m.rest)
 
 object SpecializedSwitch:
-  def unapply(b: Block) = ???
+  def unapply(b: Block) = b match
+    case m @ Match(scrut = r @ Value.Ref(l, _)) =>
+      val chain = findMatchChainRec(m, r, Nil)
+      val SwitchLikeBlock(scrut, cases, dflt, rest) = matchChainToSwitch(chain)
+      if cases.size < 2 then N
+      else
+        S((scrut, cases.reverse, dflt, rest))
+    case _ => N
+
