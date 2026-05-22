94688c5136f03a615b5f33f34542c856fdea1347
ychenfo <43136427+ychenfo@users.noreply.github.com>
2026-05-22 18:45:04 +0800

    do not generate toplevel definitions for branch/rest functions coming from class methods

--- hkmc2/shared/src/main/scala/hkmc2/codegen/deforest/Rewrite.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/deforest/Rewrite.scala
@@ -180,8 +180,10 @@ class DeforestRewriter(val solver: DeforestFusionSolver)(using Raise):
             val branchName = whichBranch.fold("_dflt")(c => s"_${c.ctorClsName}")
             val scrutName = dest._1.getReferredSym.nme
             val branchFnNme = s"${dest.instId.mkFunName}$$$scrutName$branchName"
+            val owner = pre.res.matchScrutToCtxOfMatch(dest._1).collectFirst:
+              case pre.InCtx.Cls(cls) => cls.isym
             new BlockMemberSymbol(branchFnNme, Nil, true)
-            -> new TermSymbol(Fun, N, Tree.Ident(branchFnNme))
+            -> new TermSymbol(Fun, owner, Tree.Ident(branchFnNme))
         )
         // compute the function parameters corresponding to ctor fields of branch funs
         branchFunParamFieldSyms.getOrElseUpdate(
@@ -209,8 +211,15 @@ class DeforestRewriter(val solver: DeforestFusionSolver)(using Raise):
             restFunId,
             locally:
               val restFunName = dest.instId.mkFunName + s"$$${nme}_rest"
+              val owner = matchOrLabelId match
+                case label: LabelSymbol =>
+                  pre.res.labelSymToCtxOfLabel(label).collectFirst:
+                    case pre.InCtx.Cls(cls) => cls.isym
+                case dtorId: ResultId =>
+                  pre.res.matchScrutToCtxOfMatch(dtorId).collectFirst:
+                    case pre.InCtx.Cls(cls) => cls.isym
               new BlockMemberSymbol(restFunName, Nil, true)
-              -> new TermSymbol(Fun, N, Tree.Ident(restFunName))
+              -> new TermSymbol(Fun, owner, Tree.Ident(restFunName))
           )
           val (ps, restBeforeParent) = getParentLabelOrMatchesAndRestBefore(matchOrLabelId)
           restOriginalBodiesAndParentRest.getOrElseUpdate(
@@ -357,9 +366,16 @@ class DeforestRewriter(val solver: DeforestFusionSolver)(using Raise):
       a match
       case bms: BlockMemberSymbol => Value.Ref(bms, bms.tsym)
       case _ => Value.Ref(a, N)
+    def mkFunRef(target: (BlockMemberSymbol, TermSymbol)): Path =
+      val (bms, tSym) = target
+      tSym.owner match
+      case Some(owner) =>
+        Select(Value.This(owner), Tree.Ident(bms.nme))(S(tSym))
+      case None =>
+        Value.Ref(bms, S(tSym))
     def mkCall(target: (BlockMemberSymbol, TermSymbol), args: Ls[Symbol]): Call =
       Call(
-        Value.Ref(target._1, S(target._2)),
+        mkFunRef(target),
         args.map(a => Arg(N, a.toValueRef)) ne_:: Nil
       )(true, false, false)
     def mkReturnCall(target: (BlockMemberSymbol, TermSymbol), args: Ls[Symbol]): Block =
@@ -522,7 +538,7 @@ class DeforestRewriter(val solver: DeforestFusionSolver)(using Raise):
           mkReturnCall(restFunSym, restFunArgs))
         val refreshedFvSymbols = dtorBranchFnFvs(branchId._1).map(s => s -> new VarSymbol(Tree.Ident(s"fv_${s.nme}")))
         val bodyWithCorrectSymbols = new RefreshSymbol(refreshedFvSymbols.toMap).applyBlock(actualBody)
-        FunDefn(N, bms, tSym,
+        FunDefn(tSym.owner, bms, tSym,
           branchFunParamFieldSyms(branchId).asParamList :: refreshedFvSymbols.unzip._2.asParamList :: Nil,
           bodyWithCorrectSymbols
         )(N, annotations = AffineAnnotForBranchFns :: PrivateModifier :: Nil)
@@ -545,13 +561,20 @@ class DeforestRewriter(val solver: DeforestFusionSolver)(using Raise):
             Begin(transformedOgBody, Return(Value.Lit(Tree.UnitLit(true)), false))
         val refreshedFvSymbols = restFnFvs(restFunId).map(s => s -> new VarSymbol(Tree.Ident(s"fv_${s.nme}")))
         val bodyWithCorrectSymbols = new RefreshSymbol(refreshedFvSymbols.toMap).applyBlock(actualBody)
-        FunDefn(N, bms, tsym, refreshedFvSymbols.unzip._2.asParamList :: Nil, bodyWithCorrectSymbols)(N, annotations = PrivateModifier :: Nil)
+        FunDefn(tsym.owner, bms, tsym, refreshedFvSymbols.unzip._2.asParamList :: Nil, bodyWithCorrectSymbols)(N, annotations = PrivateModifier :: Nil)
     end newRestFuns
 
     val inplaceRewrittenFunBodies = Map.from[TermSymbol, Block]:
       for (selfInstId, funSym) <- collector.synthesizedInstIdToFunSym yield
         val fDefn = pre.res.funSymToFunDefn(funSym)
         funSym -> new Rewriter(selfInstId).applyBlock(fDefn.body)
+    
+    val newTopLevelBranchFuns = newBranchFuns.filter(_.owner.isEmpty)
+    val newTopLevelRestFuns = newRestFuns.filter(_.owner.isEmpty)
+    val newClassMethods = (newBranchFuns ++ newRestFuns)
+      .groupBy(_.owner)
+      .collect:
+        case (Some(owner), methods) => owner -> methods.toList
 
     val newMainBody =
       object mainRewriter extends Rewriter(Nil):
@@ -560,15 +583,26 @@ class DeforestRewriter(val solver: DeforestFusionSolver)(using Raise):
             case Some(rewrittenBody) =>
               FunDefn(fun.owner, fun.sym, fun.dSym, fun.params, rewrittenBody)(fun.configOverride, fun.annotations)
             case None => super.applyFunDefn(fun)
+        override def applyClsLikeDefn(defn: ClsLikeDefn)(k: Defn => Block): Block =
+          super.applyClsLikeDefn(defn): transformed =>
+            transformed match
+            case cls: ClsLikeDefn =>
+              newClassMethods.get(cls.isym) match
+              case Some(methods) =>
+                k(cls.copy(methods = cls.methods ++ methods)(cls.configOverride, cls.annotations))
+              case None =>
+                k(cls)
+            case _ =>
+              k(transformed)
       object implicitRetPass extends BlockTransformerShallow(_symSubst):
         override def applyBlock(b: Block): Block = b match
           case Return(res, false) => Return(res, true)
           case _ => super.applyBlock(b)
       Scoped(
-        Set.from(newPolyFuns.map(_.sym) ++ newBranchFuns.map(_.sym) ++ newRestFuns.map(_.sym)),
+        Set.from(newPolyFuns.map(_.sym) ++ newTopLevelBranchFuns.map(_.sym) ++ newTopLevelRestFuns.map(_.sym)),
         implicitRetPass.applyBlock(mainRewriter.applyBlock(pre.pgrm.main)))
     
-    (newPolyFuns ++ newBranchFuns ++ newRestFuns).foldRight(newMainBody): (fdef, rest) =>
+    (newPolyFuns ++ newTopLevelBranchFuns ++ newTopLevelRestFuns).foldRight(newMainBody): (fdef, rest) =>
       Define(fdef, rest)
     
   end newBody
