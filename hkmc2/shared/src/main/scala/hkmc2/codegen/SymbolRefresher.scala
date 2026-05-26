725b466f5476e169d90c4edb84cace2ea4ddde77
Lionel Parreaux <lionel.parreaux@gmail.com>
2026-05-26 17:50:05 +0800

    Fix horribly wrong logic (causes 1 regression)

--- hkmc2/shared/src/main/scala/hkmc2/codegen/SymbolRefresher.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/SymbolRefresher.scala
@@ -212,7 +212,9 @@ class SymbolRefresher(existingMapping: Map[Symbol, Symbol])(using State) extends
         case Some(newBms: BlockMemberSymbol) =>
           val newDisamb = mapping.get(disamb) match
             case Some(nd: DefinitionSymbol[?]) => nd
-            case _ => newBms.tsym.getOrElse(lastWords(s"Expected disamb to be present for newBms: `$newBms`"))
+            // case _ => newBms.tsym.getOrElse(lastWords(s"Expected disamb to be present for newBms: `$newBms`"))
+            case Some(nd) => lastWords(s"unexpected symbol kind for disamb: ${nd}")
+            case N => disamb
           k(newBms.asMemberRef(newDisamb))
         case Some(newSym: (LocalVarSymbol | TempSymbol)) => k(newSym.asSimpleRef)
         case _ => super.applyValue(v)(k)
