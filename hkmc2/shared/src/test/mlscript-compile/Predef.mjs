dcf012e69eda3662077028ac9bc45b5ff1987f7d
Luyu Cheng <2239547+chengluyu@users.noreply.github.com>
2026-05-27 04:36:48 +0800

    Update golden outputs after hkmc2 merge

--- hkmc2/shared/src/test/mlscript-compile/Predef.mjs
+++ hkmc2/shared/src/test/mlscript-compile/Predef.mjs
@@ -4,7 +4,6 @@ import runtime from "./Runtime.mjs";
 import RuntimeJS from "./RuntimeJS.mjs";
 import Runtime from "./Runtime.mjs";
 import Rendering from "./Rendering.mjs";
-import Term from "./Term.mjs";
 let Predef1;
 (class Predef {
   static {
@@ -72,19 +71,6 @@ let Predef1;
     Predef.render = Rendering.render;
     Predef.js_assert = globalThis.console["assert"];
     Predef.foldl = Predef.fold;
-    (class meta {
-      static {
-        Predef.meta = this
-      }
-      static codegen(t, file) {
-        return runtime.safeCall(Term.codegen(t, file))
-      }
-      static print(t) {
-        return runtime.safeCall(Term.print(t))
-      }
-      toString() { return runtime.render(this); }
-      static [definitionMetadata] = ["class", "meta"];
-    });
   }
   static id(x) {
     return x
