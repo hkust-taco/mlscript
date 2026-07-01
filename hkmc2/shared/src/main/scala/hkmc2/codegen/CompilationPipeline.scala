61b8ad04ef16c41eff6ae1e3483de655226df8ad
Anson Yeung <nanolify@gmail.com>
2026-07-02 00:01:19 +0800

    Use List(...)

--- hkmc2/shared/src/main/scala/hkmc2/codegen/CompilationPipeline.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/CompilationPipeline.scala
@@ -24,38 +24,38 @@ class CompilationPipeline(using Config, Raise, State, Ctx, SymbolPrinter):
     if blk is prog.main then prog else Program(prog.imports, blk)
   
   def run(prog: Program, printer: Program => Str, symbolsToPreserve: Set[BoundSymbol], otl: TL)(using TL): Program =
-    val allPasses =
-      CompilationPass("LambdaRewriter", LambdaRewriter.desugar) ::
+    val allPasses = List(
+      CompilationPass("LambdaRewriter", LambdaRewriter.desugar),
       CompilationPass("Deforest", prog =>
         val outterTl = tl
         config.deforest match
           case None => prog
           case Some(dCfg) =>
             flowAnalysis.FlowAnalysis.mkTraceLogger(dCfg.config, "deforest > ", outterTl).givenIn:
-              deforest.Deforest(prog)) ::
-      CompilationPass("EtaExpansion", EtaExpansion.apply) ::
+              deforest.Deforest(prog)),
+      CompilationPass("EtaExpansion", EtaExpansion.apply),
       CompilationPass("Lifter", prog =>
         if config.liftDefns.isDefined then
           blockPass(Lifter(_).transform)(prog)
-        else prog) ::
+        else prog),
       CompilationPass("HandlerLowering", prog =>
         config.effectHandlers.fold(prog): opt =>
-          HandlerLowering(new HandlerPaths, opt).translateProgram(prog)) ::
-      CompilationPass("Flattening", blockPass(_.flattened)) ::
-      CompilationPass("BufferableTransform", BufferableTransform().transform) ::
-      CompilationPass("MergeMatchArmTransformer", MergeMatchArmTransformer.applyProgram) ::
+          HandlerLowering(new HandlerPaths, opt).translateProgram(prog)),
+      CompilationPass("Flattening", blockPass(_.flattened)),
+      CompilationPass("BufferableTransform", BufferableTransform().transform),
+      CompilationPass("MergeMatchArmTransformer", MergeMatchArmTransformer.applyProgram),
       CompilationPass("FirstClassFunctionTransformer", prog =>
         if config.funcToCls then
           blockPass(blk => Lifter(FirstClassFunctionTransformer().transform(blk)).transform)(prog)
-        else prog) ::
-      CompilationPass("ClassParamFlattener", ClassParamFlattener.apply) ::
-      CompilationPass("ReflectionInstrumenter", ReflectionInstrumenter(using summon).apply) ::
-      CompilationPass("TailRecOpt", TailRecOpt().transform) ::
-      CompilationPass("PreOptimizeHook", preOptimizeHook) ::
-      CompilationPass("WorkerWrapper", WorkerWrapper(symbolsToPreserve, otl, printer)) ::
-      CompilationPass("BlockSimplifier", BlockSimplifier(symbolsToPreserve, otl, printer).apply) ::
-      CompilationPass("DeadParamElim", otl.givenIn(DeadParamElim.apply)) ::
-      Nil
+        else prog),
+      CompilationPass("ClassParamFlattener", ClassParamFlattener.apply),
+      CompilationPass("ReflectionInstrumenter", ReflectionInstrumenter(using summon).apply),
+      CompilationPass("TailRecOpt", TailRecOpt().transform),
+      CompilationPass("PreOptimizeHook", preOptimizeHook),
+      CompilationPass("WorkerWrapper", WorkerWrapper(symbolsToPreserve, otl, printer)),
+      CompilationPass("BlockSimplifier", BlockSimplifier(symbolsToPreserve, otl, printer).apply),
+      CompilationPass("DeadParamElim", otl.givenIn(DeadParamElim.apply)),
+    )
     allPasses.foldLeft(prog): (before, pass) =>
       val after = pass.transform(before)
       passHook(pass, before, after)
